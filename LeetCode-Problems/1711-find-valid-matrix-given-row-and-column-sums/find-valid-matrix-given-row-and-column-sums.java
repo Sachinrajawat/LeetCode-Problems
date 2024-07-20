0ec626c73721dc810efa2573fac888734b1fbfcd
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-07-20 06:32:50 +0530

    Time: 4 ms (80.08%) | Memory: 53 MB (26.69%) - LeetSync

---
+++ LeetCode-Problems/1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int m=rowSum.length,n=colSum.length;
+
+        int[][] ans=new int[m][n];
+        for(int i=0;i<m;i++){
+            int x=rowSum[i];
+            for(int j=0;j<n;j++){
+                if(x<=colSum[j]){
+                    ans[i][j]=x;
+                    colSum[j]-=x;
+                    x=0;
+                }
+                else{
+                    ans[i][j]=colSum[j];
+                    x-=colSum[j];
+                    colSum[j]=0;
+                }
+
+            }
+        }
+
+        return ans;
+    }
+}
