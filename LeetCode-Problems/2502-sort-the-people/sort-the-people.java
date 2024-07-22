365285bf20ec25cc8fe67762c7daa9312e4f179f
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-07-22 07:01:29 +0530

    Time: 9 ms (53.75%) | Memory: 45.3 MB (50.36%) - LeetSync

---
+++ LeetCode-Problems/2502-sort-the-people/sort-the-people.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        TreeMap<Integer,String> tr=new TreeMap<>(Collections.reverseOrder());
+        for(int i=0;i<heights.length;i++){
+            tr.put(heights[i],names[i]);
+        }
+        int j=0;
+        for(var key:tr.keySet()){
+            names[j++]=tr.get(key);
+        }
+        return names;
+    }
+}
