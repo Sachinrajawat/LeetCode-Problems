8fe5c201a4c326afebdeca1406e49a8c4f394b41
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-07-23 14:10:14 +0530

    Time: 9 ms (56.79%) | Memory: 45.4 MB (89.01%) - LeetSync

---
+++ LeetCode-Problems/3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean doesAliceWin(String st) {
+        StringBuilder s=new StringBuilder(st);
+        int v=0;
+        for(int i=0;i<st.length();i++){
+            char ch=st.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                v++;
+            }
+        }
+        if(v==0) return false;
+        else if(v%2!=0) return true;
+        System.out.println(v);
+        return true;
+    }
+}
