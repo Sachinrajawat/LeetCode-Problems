ad608105aabcee2cfb4d14941058026a4dfae3d5
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-08-14 10:42:51 +0530

    Time: 574 ms (5.10%) | Memory: 44.9 MB (5.10%) - LeetSync

--- LeetCode-Problems/719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ LeetCode-Problems/719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -1,20 +1,5 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
-/*
-        ArrayList<Integer> list=new ArrayList<>();
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                int a=Math.abs(nums[i]-nums[j]);
-                max=Math.max(a,max);
-                if(list.size()<=k || a<=max) list.add(a);
-            }
-        }
-        System.out.println(list);
-        Collections.sort(list);
-        int ans=list.get(k-1);
-        return ans;
-        */
         int n=nums.length,ans=0;
         Arrays.sort(nums);
         int start=0,end=nums[n-1]-nums[0];
@@ -32,9 +17,7 @@ class Solution {
                 ans=mid;
                 end=mid-1;
             }
-            else{
-                start=mid+1;
-            }
+            else start=mid+1;
         }
         return ans;
     }
