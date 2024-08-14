5ace570bfeea54669e827e5dccce1c2cb9b4dadd
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-08-14 11:16:06 +0530

    Time: 13 ms (30.11%) | Memory: 45.2 MB (16.11%) - LeetSync

---
+++ LeetCode-Problems/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    ArrayList LCS(TreeNode root,ArrayList<TreeNode> arr,TreeNode target){
+        if(root==target){
+            arr.add(root);
+            return arr;
+        }
+        else{
+            arr.add(root);
+        }
+        if(target.val<root.val) LCS(root.left,arr,target);
+        if(target.val>root.val) LCS(root.right,arr,target);
+        return arr;
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        ArrayList<TreeNode> pLCS=new ArrayList<>();
+        LCS(root,pLCS,p);
+        // System.out.println(pLCS);
+        for(TreeNode n:pLCS){
+            System.out.print(n.val+" ");
+        }
+        ArrayList<TreeNode> qLCS=new ArrayList<>();
+        LCS(root,qLCS,q);
+        for(TreeNode n:qLCS){
+            System.out.print(n.val+" ");
+        }
+        for(int i=pLCS.size()-1;i>=0;i--){
+            for(int j=qLCS.size()-1;j>=0;j--){
+                if(pLCS.get(i)==qLCS.get(j)) return pLCS.get(i);
+            }
+        }
+        return root;
+    }
+}
