f2371988123144089228bb9c8fd4a15411f1b979
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-08-15 09:57:19 +0530

    Time: 3 ms (14.05%) | Memory: 55.9 MB (36.83%) - LeetSync

--- LeetCode-Problems/890-lemonade-change/lemonade-change.java
+++ LeetCode-Problems/890-lemonade-change/lemonade-change.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        if(bills[0]>5) return false;
         int sum=0;
         int[] arr={0,0,0}; //count of $5,$10,$20
         for(int i=0;i<bills.length;i++){
             if(bills[i]==10){
-                arr[0]--;
+                arr[0]--; //decrease $5 count
                 if(arr[0]<0) return false;
-                arr[1]++;
+                arr[1]++; //increase $10 count
             }
             else if(bills[i]==20){
                 if(arr[1]>=1){ //if $10 coin present then take $5 & $10 coin
@@ -16,12 +15,9 @@ class Solution {
                 }
                 else arr[0]-=3; //if $10 coin not present then take three $5 coin
                 if(arr[0]<0 ||arr[1]<0) return false; 
-                arr[2]++;
+                arr[2]++; //increase $20 count
             }
-            else arr[0]++;
-            // if(bills[i]==5) arr[0]++;
-            // else if(bills[i]==10) arr[1]++;
-            // else arr[2]++;
+            else arr[0]++; //increase $5 count
         }
         return true;
     }
