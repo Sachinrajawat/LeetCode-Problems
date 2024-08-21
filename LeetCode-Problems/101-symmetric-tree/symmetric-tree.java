45f1bda8af2e9429d52c7f0c8cc79a47ec7161c7
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-08-21 08:35:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (50.54%) - LeetSync

--- LeetCode-Problems/101-symmetric-tree/symmetric-tree.java
+++ LeetCode-Problems/101-symmetric-tree/symmetric-tree.java
@@ -17,12 +17,9 @@ class Solution {
     public boolean isEqual(TreeNode r1,TreeNode r2){
         if(r1==null || r2==null) return r1==r2;
         if(r1.val!=r2.val) return false;
-        
-        
         return isEqual(r1.right,r2.left)&&isEqual(r1.left,r2.right);
     }
     public boolean isSymmetric(TreeNode root) {
-        // if(root.left.val!=root.right.val) return false;
         return root==null || isEqual(root.left,root.right);
     }
 }
