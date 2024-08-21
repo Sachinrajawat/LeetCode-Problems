85c366b3b304e25e83100380736b871ca5e3d307
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-08-21 08:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (66.58%) - LeetSync

---
+++ LeetCode-Problems/101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEqual(TreeNode r1,TreeNode r2){
+        if(r1==null || r2==null) return r1==r2;
+        if(r1.val!=r2.val) return false;
+        
+        
+        return isEqual(r1.right,r2.left)&&isEqual(r1.left,r2.right);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        // if(root.left.val!=root.right.val) return false;
+        return root==null || isEqual(root.left,root.right);
+    }
+}
