4491d38b521aecde6dfa0ae388bbfba9e9aa862b
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-08-23 11:52:41 +0530

    Time: 8 ms (45.87%) | Memory: 42.2 MB (33.49%) - LeetSync

--- LeetCode-Problems/592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
+++ LeetCode-Problems/592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
@@ -7,24 +7,26 @@ class Solution {
         }
         return num1;
     }
+
     public String fractionAddition(String expression) {
         String[] nums = expression.split("/|(?=[-+])");
-        int deno=1;
-        for(int i=1;i<nums.length;i+=2){
-            deno*=Integer.parseInt(nums[i]);
+        int deno = 1;
+        for (int i = 1; i < nums.length; i += 2) {
+            deno *= Integer.parseInt(nums[i]);
         }
-        int nume=0;
-        for(int i=0;i<nums.length-1;i+=2){
-            nume+=Integer.parseInt(nums[i])*(deno/Integer.parseInt(nums[i+1]));
+        int nume = 0;
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            nume += Integer.parseInt(nums[i]) * (deno / Integer.parseInt(nums[i + 1]));
         }
-        
-        int gcd=findGCD(nume,deno);
-        if(gcd<0) gcd*=-1;
-        nume/=gcd;
-        deno/=gcd;
 
-        String ans="";
-        ans=String.valueOf(nume)+'/'+String.valueOf(deno);
+        int gcd = findGCD(nume, deno);
+        if (gcd < 0)
+            gcd *= -1;
+        nume /= gcd;
+        deno /= gcd;
+
+        String ans = "";
+        ans = String.valueOf(nume) + '/' + String.valueOf(deno);
         return ans;
     }
 }
