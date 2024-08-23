624018f0db2ba9de6b731854c6931f4ab3371261
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-08-23 11:26:42 +0530

    Time: 9 ms (36.24%) | Memory: 41.9 MB (48.17%) - LeetSync

--- LeetCode-Problems/592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
+++ LeetCode-Problems/592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
@@ -13,21 +13,14 @@ class Solution {
         for(int i=1;i<nums.length;i+=2){
             deno*=Integer.parseInt(nums[i]);
         }
-        // System.out.println(deno);
         int nume=0;
         for(int i=0;i<nums.length-1;i+=2){
             nume+=Integer.parseInt(nums[i])*(deno/Integer.parseInt(nums[i+1]));
         }
-        // System.out.println(nume);
-        // for(int i=0;i<nums.length;i++){
-        //     System.out.println(nums[i]);
-        // }
         int gcd=findGCD(nume,deno);
-        // System.out.println(gcd);
         if(gcd<0) gcd*=-1;
         nume/=gcd;
         deno/=gcd;
-        // System.out.println(nume+" "+deno);
         String ans="";
         ans=String.valueOf(nume)+'/'+String.valueOf(deno);
         
