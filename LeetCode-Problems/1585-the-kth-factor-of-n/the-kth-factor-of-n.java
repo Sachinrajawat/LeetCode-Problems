aebdd04e879fa8b7c98a3368e9a6424b0b6bcc03
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-08-29 19:22:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (62.69%) - LeetSync

--- LeetCode-Problems/1585-the-kth-factor-of-n/the-kth-factor-of-n.java
+++ LeetCode-Problems/1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -1,35 +1,29 @@
 class Solution {
-    public boolean isPrime(int n){
-        if(n==1) return false;
-        if(n==2 || n==3) return true;
-        if(n%2==0||n%3==0) return false;
-        for(int i=5;i<Math.sqrt(n);i+=6){
-            if((n%i==0)||(n%(i+2)==0)) return false;
-        }
-        return true;
-    }
+    // public boolean isPrime(int n){
+    //     if(n==1) return false;
+    //     if(n==2 || n==3) return true;
+    //     if(n%2==0||n%3==0) return false;
+    //     for(int i=5;i<Math.sqrt(n);i+=6){
+    //         if((n%i==0)||(n%(i+2)==0)) return false;
+    //     }
+    //     return true;
+    // }
     public int kthFactor(int n, int k) {
         if(k==1) return 1;
-        if(isPrime(n)){
-            if(k>2) return -1;
-            else if(k==2) return n;
-        }
+        // if(isPrime(n)){
+        //     if(k>2) return -1;
+        //     else if(k==2) return n;
+        // }
         
         if(n/2+1<k) return -1;
-        // ArrayList<Integer> arr=new ArrayList<>();
-        // arr.add(1);
         int a=1;
         for(int i=2;i<=n/2;i++){
             if(n%i==0){
                 a++;
-                System.out.println(a+" "+i);
                 if(a==k) return i;
             }
         }
         if(a+1==k) return n;
-        // arr.add(n);
-        // System.out.println(arr);
-        // if(arr.size()<k) return -1;
         return -1;
     }
 }
