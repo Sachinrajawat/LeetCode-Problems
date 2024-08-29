659c31ad4240253d2503d2da0e1ceac9fb35bd2f
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-08-29 11:32:48 +0530

    Time: 11 ms (7.22%) | Memory: 45.3 MB (72.20%) - LeetSync

---
+++ LeetCode-Problems/508-most-frequent-subtree-sum/most-frequent-subtree-sum.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sum(HashMap<Integer,Integer> map,TreeNode root){
+        if(root==null) return 0;
+        int sum=root.val+sum(map,root.left)+sum(map,root.right);
+        map.put(sum,map.getOrDefault(sum,0)+1);
+        return sum;
+    }
+    public int[] findFrequentTreeSum(TreeNode root) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        sum(map,root);
+        System.out.println(map);
+        
+        int maxFreq=1;
+        int count=0;
+        for(var key:map.keySet()){
+            if(maxFreq==map.get(key)) count++;
+            if(maxFreq<map.get(key)){
+            maxFreq=map.get(key);
+            count=1;
+           }
+            
+        }
+        int[] arr=new int[count];
+        int i=0;
+        for(var key:map.keySet()){
+            if(maxFreq==map.get(key)){
+                arr[i++]=key;
+            }
+        }
+        return arr;
+    }
+}
