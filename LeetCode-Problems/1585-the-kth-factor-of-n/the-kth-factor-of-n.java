d4b47e1b4b15fda2718c5d357ef0ee38a39a1864
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-08-29 19:23:02 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (17.25%) - LeetSync

--- LeetCode-Problems/1585-the-kth-factor-of-n/the-kth-factor-of-n.java
+++ LeetCode-Problems/1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -1,20 +1,6 @@
 class Solution {
-    // public boolean isPrime(int n){
-    //     if(n==1) return false;
-    //     if(n==2 || n==3) return true;
-    //     if(n%2==0||n%3==0) return false;
-    //     for(int i=5;i<Math.sqrt(n);i+=6){
-    //         if((n%i==0)||(n%(i+2)==0)) return false;
-    //     }
-    //     return true;
-    // }
     public int kthFactor(int n, int k) {
         if(k==1) return 1;
-        // if(isPrime(n)){
-        //     if(k>2) return -1;
-        //     else if(k==2) return n;
-        // }
-        
         if(n/2+1<k) return -1;
         int a=1;
         for(int i=2;i<=n/2;i++){
