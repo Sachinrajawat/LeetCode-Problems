8b430d7e4bdb1fb286cc1b7a29f93b47f839466b
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-01 07:58:03 +0530

    Time: 3 ms (97.73%) | Memory: 55.5 MB (36.41%) - LeetSync

--- LeetCode-Problems/2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ LeetCode-Problems/2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -4,9 +4,8 @@ class Solution {
         int[][]  arr=new int[m][n];
         int k=0;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<n;j++)
                 arr[i][j]=original[k++];
-            }
         }
         return arr;
     }
