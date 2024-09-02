05211a4ed34b57729bfc4465c5e46944cb068500
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-02 08:34:04 +0530

    Time: 2 ms (72.59%) | Memory: 57.2 MB (95.42%) - LeetSync

--- LeetCode-Problems/2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
+++ LeetCode-Problems/2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -3,24 +3,17 @@ class Solution {
         int n=chalk.length;
         long[] prefixSum=new long[n];
         prefixSum[0]=chalk[0];
-        for(int i=1;i<n;i++){
-            prefixSum[i]=prefixSum[i-1]+chalk[i];
-        }
-        for(int i=0;i<n;i++){
-            System.out.print(prefixSum[i]+" ");
-        }
+        for(int i=1;i<n;i++) prefixSum[i]=prefixSum[i-1]+chalk[i];
+        
         k%=prefixSum[n-1];
-        int start=0,end=chalk.length-1;
-        int ans=0;
+        int start=0, end=chalk.length-1, ans=0;
         while(start<=end){
             int mid=start+(end-start)/2;
             if(prefixSum[mid]>k){
                 end=mid-1;
                 ans=mid;
             }
-            else{
-                start=mid+1;
-            }
+            else start=mid+1;
         }
         return ans;
     }
