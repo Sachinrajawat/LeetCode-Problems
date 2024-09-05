988589022a16faf9e44c2ac2cc7d8d57baaea5e3
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-05 09:33:12 +0530

    Time: 4 ms (71.80%) | Memory: 61.7 MB (40.66%) - LeetSync

--- LeetCode-Problems/2155-find-missing-observations/find-missing-observations.java
+++ LeetCode-Problems/2155-find-missing-observations/find-missing-observations.java
@@ -2,13 +2,14 @@ class Solution {
     public int[] missingRolls(int[] rolls, int mean, int n) {
         int sum=0,m=rolls.length;
         for(int i:rolls) sum+=i;
-        int nSum=mean*(n+m)-sum;
+        int missSum=mean*(n+m)-sum;
 
-        if(nSum>6*n || nSum<0 ||n>nSum) return new int[0];
+        if(missSum>6*n || missSum<0 ||n>missSum) return new int[0];
+        
         int[] ans=new int[n];
-        int rem=nSum%n, j=0;
+        int rem=missSum%n, j=0;
         for(int i=0;i<ans.length;i++){
-            ans[i]+=nSum/n;
+            ans[i]+=missSum/n;
             if(rem!=0){
                 if(6-ans[j]<=rem){
                     rem-=6-ans[j];
