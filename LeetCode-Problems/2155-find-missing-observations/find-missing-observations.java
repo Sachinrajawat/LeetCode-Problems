3ad6a9d72923e2e75b51bc492bf9b4538f7dd5d1
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-05 09:23:45 +0530

    Time: 5 ms (31.80%) | Memory: 62 MB (35.08%) - LeetSync

--- LeetCode-Problems/2155-find-missing-observations/find-missing-observations.java
+++ LeetCode-Problems/2155-find-missing-observations/find-missing-observations.java
@@ -4,12 +4,9 @@ class Solution {
         for(int i:rolls) sum+=i;
         int nSum=mean*(n+m)-sum;
 
-        System.out.println(nSum);
         if(nSum>6*n || nSum<0 ||n>nSum) return new int[0];
         int[] ans=new int[n];
-        for(int i = 0; i < ans.length; i++) {
-            ans[i] += nSum/n;
-        }
+        for(int i=0;i<ans.length;i++) ans[i]+=nSum/n;
         int rem=nSum%n;
         if(rem!=0){
             int j=0;
@@ -20,14 +17,10 @@ class Solution {
                 }
                 else{
                     ans[j++]+=rem;
-                    rem=0;
+                    break;
                 }
             }
         }
-        // for(int i = 0; i < ans.length; i++) {
-        //     System.out.print(ans[i]+" ");
-        // }
-        System.out.println(nSum);
         return ans;
     }
 }
