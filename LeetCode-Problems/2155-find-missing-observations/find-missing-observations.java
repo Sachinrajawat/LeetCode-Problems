4928b4c2325284da0114a68ed7ea8bbfa0998c58
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-05 14:03:51 +0530

    Time: 5 ms (31.80%) | Memory: 62.1 MB (21.97%) - LeetSync

--- LeetCode-Problems/2155-find-missing-observations/find-missing-observations.java
+++ LeetCode-Problems/2155-find-missing-observations/find-missing-observations.java
@@ -4,21 +4,15 @@ class Solution {
         for(int i:rolls) sum+=i;
         int missSum=mean*(n+m)-sum;
 
-        if(missSum>6*n || missSum<0 ||n>missSum) return new int[0];
+        if(missSum>6*n || missSum<n) return new int[0];
         
         int[] ans=new int[n];
         int rem=missSum%n, j=0;
         for(int i=0;i<ans.length;i++){
             ans[i]+=missSum/n;
-            if(rem!=0){
-                if(6-ans[j]<=rem){
-                    rem-=6-ans[j];
-                    ans[j++]=6;
-                }
-                else{
-                    ans[j]+=rem;
-                    rem=0;
-                }
+            if(rem>0){
+                ans[i]+=1;
+                rem--;
             }
         }
         return ans;
