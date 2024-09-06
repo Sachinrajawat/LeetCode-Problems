84d6e1ec18808c68a0556884fff25f131e6f1f15
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-06 19:31:16 +0530

    Time: 6 ms (33.70%) | Memory: 44.3 MB (26.17%) - LeetSync

---
+++ LeetCode-Problems/71-simplify-path/simplify-path.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] arr=path.split("/");
+        int j=0;
+        String ans="";
+        // for(String i:arr) System.out.println(i);
+        Stack<String> st=new Stack<>();
+        st.push("/");
+        for(int i=0;i<arr.length;i++){
+            if(!st.peek().equals("/")) st.push("/");
+            if(arr[i].equals(".") || arr[i].equals("")){
+                continue;
+            }
+            else if(arr[i].equals("..")){
+                if(st.size()>1) st.pop();
+                if(st.size()>1) st.pop();
+            }
+            else{
+                st.push(arr[i]);
+            }
+
+        }
+        if(st.size()>1 && st.peek().equals("/")) st.pop(); 
+        while(!st.isEmpty()){
+            ans=st.pop()+ans;
+        }
+        return ans;
+    }
+}
