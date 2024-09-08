add4f8d8f7e9513397a85e4cb3955c6907a2cb1a
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-08 11:31:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (70.75%) - LeetSync

--- LeetCode-Problems/725-split-linked-list-in-parts/split-linked-list-in-parts.java
+++ LeetCode-Problems/725-split-linked-list-in-parts/split-linked-list-in-parts.java
@@ -11,40 +11,35 @@
 class Solution {
     public ListNode[] splitListToParts(ListNode head, int k) {
         ListNode[] arr=new ListNode[k];
-        arr[0]=head;
         ListNode temp=head;
         int count=0;
         while(temp!=null){
             count++;
             temp=temp.next;
         }
-        int[] ar=new int[k];
-        int x=count/k;
-        for(int i=0;i<k;i++){
-            ar[i]=x;
-        }
+        int[] size=new int[k]; //size of each list
         int y=count%k;
-        int j=0;
-        while(y>0){
-            ar[j++]+=1;
-            y--;
+        for(int i=0;i<k;i++){
+            size[i]=count/k;
+            if(y>0){
+                size[i]+=1;
+                y--;
+            }
         }
-        for(int i:ar) System.out.print(i+" ");
+        
         temp=head;
         for(int i=0;i<k;i++){
             ListNode prev=null;
             arr[i]=temp;
-            if(ar[i]==0) arr[i]=prev;
-            else{
-            while(ar[i]>0){
-                prev=temp;
-                temp=temp.next;
-                ar[i]--;
+            if(size[i]>0){
+                while(size[i]>0){
+                    prev=temp;
+                    temp=temp.next;
+                    size[i]--;
+                    }
+                    prev.next=null;
                 }
-                prev.next=null;
             }
-        }
-        // System.out.println(count);
         return arr;
     }
 }
