29216d4383d9e9e25d97e7a0f3b6c8ac561722f7
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-09 10:53:09 +0530

    Time: 7 ms (87.82%) | Memory: 56.8 MB (49.51%) - LeetSync

--- LeetCode-Problems/2411-spiral-matrix-iv/spiral-matrix-iv.java
+++ LeetCode-Problems/2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -14,49 +14,37 @@ class Solution {
         ListNode temp=head;
         int top=0,bottom=m-1,left=0,right=n-1;
         while(top<=bottom && left<=right){
-             System.out.println("Top: ");
-        for(int i=left;i<=right;i++){
-            if(temp!=null){
-                mat[top][i]=temp.val;
-                // System.out.println(mat[top][i]);
-                temp=temp.next;
+            for(int i=left;i<=right;i++){
+                if(temp!=null){
+                    mat[top][i]=temp.val;
+                    temp=temp.next;
+                }
+                else mat[top][i]=-1;
             }
-            else mat[top][i]=-1;
-            System.out.print(mat[top][i]+" ");
-            
-        }
-        System.out.println("Right: ");
-        
             for(int i=top+1;i<=bottom;i++){
-            if(temp!=null){
-                mat[i][right]=temp.val;
-                temp=temp.next;
+                if(temp!=null){
+                    mat[i][right]=temp.val;
+                    temp=temp.next;
+                }
+                else mat[i][right]=-1;
             }
-            else mat[i][right]=-1;
-            System.out.println(mat[i][right]+" ");
-        }
-        
-         System.out.println("Botom: ");
         if(bottom!=top){
             for(int i=right-1;i>=left;i--){
-            if(temp!=null){
-                mat[bottom][i]=temp.val;
-                temp=temp.next;
+                if(temp!=null){
+                    mat[bottom][i]=temp.val;
+                    temp=temp.next;
+                }
+                else mat[bottom][i]=-1;
             }
-            else mat[bottom][i]=-1;
-            System.out.println(mat[bottom][i]+" ");
         }
-        }
-        System.out.println("Left: ");
         if(right!=left){
             for(int i=bottom-1;i>top;i--){
-            if(temp!=null){
-                mat[i][left]=temp.val;
-                temp=temp.next;
+                if(temp!=null){
+                    mat[i][left]=temp.val;
+                    temp=temp.next;
+                }
+                else mat[i][left]=-1;
             }
-            else mat[i][left]=-1;
-            System.out.println(mat[i][left]+" ");
-        }
         }
         top++;
         bottom--;
