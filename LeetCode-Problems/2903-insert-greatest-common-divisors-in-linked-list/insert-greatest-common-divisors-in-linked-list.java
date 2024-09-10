25bf16e7578793f9f5f02623b0c6a9ca59d1cd05
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-10 08:19:22 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (97.59%) - LeetSync

--- LeetCode-Problems/2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ LeetCode-Problems/2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -14,12 +14,12 @@ class Solution {
             return gcd(b,a%b);
         }
     public ListNode insertGreatestCommonDivisors(ListNode head) {
-        if(head.next==null) return head;
+        // if(head.next==null) return head;
         ListNode curr=head;
         while(curr.next!=null){
-            // int Gcd=gcd(curr.val,curr.next.val);
+            int Gcd=gcd(curr.val,curr.next.val);
             ListNode temp=curr.next;
-            curr.next=new ListNode(gcd(curr.val,curr.next.val));
+            curr.next=new ListNode(Gcd);
             curr.next.next=temp;
             curr=temp;
         }
