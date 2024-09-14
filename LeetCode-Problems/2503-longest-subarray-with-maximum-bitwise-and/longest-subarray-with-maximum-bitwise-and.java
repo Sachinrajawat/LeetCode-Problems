f3650252f98536d4e2634927294d8336c986649d
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-14 10:06:33 +0530

    Time: 3 ms (89.89%) | Memory: 59.6 MB (38.76%) - LeetSync

--- LeetCode-Problems/2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
+++ LeetCode-Problems/2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -6,14 +6,12 @@ class Solution {
         }
         int len=0;
         int count=1;
-        System.out.println(max);
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]==max && nums[i]==nums[i+1]) count++;
             else{
                 len=Math.max(len,count);
                 count=1;
             }
-            System.out.println(count+" "+len);
         }
         len=Math.max(count,len);
         return len;
