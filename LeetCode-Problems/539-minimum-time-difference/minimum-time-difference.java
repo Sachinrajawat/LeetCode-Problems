d0ce74e57c92ef54b2416e618da7fd6fb8e969f6
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-16 11:10:24 +0530

    Time: 9 ms (43.90%) | Memory: 45.4 MB (63.14%) - LeetSync

--- LeetCode-Problems/539-minimum-time-difference/minimum-time-difference.java
+++ LeetCode-Problems/539-minimum-time-difference/minimum-time-difference.java
@@ -1,19 +1,12 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
         int ans=Integer.MAX_VALUE;
-        // ArrayList<Integer> arr=new ArrayList<>();
         PriorityQueue<Integer> pq=new PriorityQueue<>();
         for(int i=0;i<timePoints.size();i++){
             int min=Integer.parseInt(timePoints.get(i).substring(0,2))*60;
             min+=Integer.parseInt(timePoints.get(i).substring(3,5));
-            // arr.add(min);
             pq.add(min);
         }
-        // Collections.sort(arr);
-        // for(int i=0;i<arr.size()-1;i++){
-        //     int x=arr.get(i+1)-arr.get(i);
-        //     ans=Math.min(ans,x);
-        // }
         int first=pq.peek(),last=0;
         int prev=pq.remove();
         while(pq.size()>0){
@@ -22,9 +15,8 @@ class Solution {
             prev=pq.remove();
             ans=Math.min(ans,x);
         }
-        // int circularDiff=1440+arr.get(0)-arr.get(arr.size()-1);
         int circularDiff=1440+first-last;
-        ans = Math.min(ans, circularDiff);
+        ans=Math.min(ans,circularDiff);
         return ans;
     }
 }
