9a9bdb9aedc8b49ffb57201e88cb2c0d7f929279
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-16 15:39:39 +0530

    Time: 14 ms (88.97%) | Memory: 47.6 MB (49.89%) - LeetSync

---
+++ LeetCode-Problems/697-degree-of-an-array/degree-of-an-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+        if(nums.length==1) return 1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        if(map.size()==nums.length) return 1;
+        int deg=0;
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(var key:map.keySet()){
+            if(map.get(key)>deg){
+                deg=map.get(key);
+                arr=new ArrayList<>();
+                arr.add(key);
+            }
+            else if(map.get(key)==deg){
+                arr.add(key);
+            }
+        }
+        System.out.println(arr);
+        int k=0;
+        int ans=Integer.MAX_VALUE;
+        while(k<arr.size()){
+            int start=-1,end=-1;
+            for(int i=0;i<nums.length;i++){
+                if(start==-1 && nums[i]==arr.get(k)){
+                    start=i;
+                }
+                else if(start!=-1 && nums[i]==arr.get(k)){
+                    end=i;
+                }
+            }
+            ans=Math.min(ans,end-start+1);
+            k++;
+        }
+        return ans;
+    }
+}
