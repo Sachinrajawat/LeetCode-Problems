10c02d021a0f738573c5a10b9daa16852a6cc73e
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-19 20:03:16 +0530

    Time: 1 ms (63.32%) | Memory: 42 MB (22.13%) - LeetSync

--- LeetCode-Problems/68-text-justification/text-justification.java
+++ LeetCode-Problems/68-text-justification/text-justification.java
@@ -1,12 +1,6 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> list=new ArrayList<>();
-        // list.add("Science  is  what we");
-        // list.add("understand      well");
-        // list.add("enough to explain to");
-        // list.add("a  computer.  Art is");
-        // list.add("everything  else  we");
-        // list.add("do                  ");
         int i=0;
         while(i<words.length){
             int k=i,width=0,word=0;
@@ -17,7 +11,6 @@ class Solution {
             }
             
             width--;
-            System.out.println(width+" "+word);
             if(i+word<=words.length-1){
             int space=1;
             if(word>1){
@@ -28,7 +21,6 @@ class Solution {
                 space+=(maxWidth-width)-1;
                 width=0;
             }
-            System.out.println(space+" "+width);
             String s="";
             for(int j=0;j<word;j++){
                 s+=words[i+j];
@@ -51,10 +43,8 @@ class Solution {
                 while(i+k<words.length){
                     s+=words[i+k];
                     if(i+k<words.length-1) s+=" ";
-                    // count+=words[i+k].length();
                     k++;
                 }
-                System.out.println(s);
                 int count=s.length();
                 while(count<maxWidth){
                     s+=" ";
