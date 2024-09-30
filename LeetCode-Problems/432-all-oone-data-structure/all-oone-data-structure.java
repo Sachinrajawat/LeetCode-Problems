1a69ba70a3410ffb18940cc0cb4357712e3aa0ab
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-09-30 15:28:37 +0530

    Time: 49 ms (88.53%) | Memory: 57.9 MB (99.63%) - LeetSync

--- LeetCode-Problems/432-all-oone-data-structure/all-oone-data-structure.java
+++ LeetCode-Problems/432-all-oone-data-structure/all-oone-data-structure.java
@@ -1,34 +1,111 @@
 class AllOne {
-    private Map<String, Integer> count;
-    private TreeSet<Pair<Integer, String>> set;
-
-    public AllOne() {
-        count = new HashMap<>();
-        set = new TreeSet<>((a, b) -> a.getKey().equals(b.getKey()) ? a.getValue().compareTo(b.getValue()) : a.getKey() - b.getKey());
-    }
-
-    public void inc(String key) {
-        int n = count.getOrDefault(key, 0);
-        count.put(key, n + 1);
-        set.remove(new Pair<>(n, key));
-        set.add(new Pair<>(n + 1, key));
-    }
-
-    public void dec(String key) {
-        int n = count.get(key);
-        set.remove(new Pair<>(n, key));
-        if (n == 1) count.remove(key);
-        else {
-            count.put(key, n - 1);
-            set.add(new Pair<>(n - 1, key));
+    class Node{
+        int count;
+        ArrayList<String> keys;
+        Node next;
+        Node prev;
+        Node(int c){
+            count=c;
+            prev=null;
+            next=null;
+            keys=new ArrayList<>();
         }
     }
-
-    public String getMaxKey() {
-        return set.isEmpty() ? "" : set.last().getValue();
+    HashMap<String,Node> mp;
+    Node head;
+    // Node first   min key string
+    Node last;
+    public AllOne() {
+        head=new Node(0);
+        last=head;
+        mp=new HashMap<>();
     }
+    void addNode(Node prevNode,int count){
+        Node newNode =new Node(count);
+        newNode.next=prevNode.next;
+        newNode.prev = prevNode;
+        if(newNode.next!=null){
+            newNode.next.prev=newNode;
+        }
+        prevNode.next=newNode;
+        if(prevNode==last){
+            last=newNode;
+        }
+    }
+    void removeNode(Node node){
+        node.prev.next=node.next;
+        if(node.next!=null){
+            node.next.prev=node.prev;
+        }
+        if(last==node){
+            last=node.prev;
+        }
+        node.prev=null;
+        node.next=null;
+    }
+    
+    public void inc(String key) {
+        if(!mp.containsKey(key)){
+            //nor exists
+            //check if there is already a node with count =1
+            if(head.next==null || head.next.count!=1){
+                addNode(head,1);
+            }
+            head.next.keys.add(key);
+            mp.put(key,head.next);
+        }
+        else{
+            //increase it frequency  -> move to +1 freq node
+            Node currNode=mp.get(key);
+            int currCount=currNode.count;
+            if(currNode.next==null || currNode.next.count!=currCount+1){
+                addNode(currNode,currCount+1);
+            }
+            currNode.next.keys.add(key);
+            mp.put(key,currNode.next);
+            currNode.keys.remove(key);
+            if(currNode.keys.isEmpty()){
+                removeNode(currNode);
+            }
+        }
+    }
+    
+    public void dec(String key) {
+        Node currNode=mp.get(key);
+        int currCount=currNode.count;
 
+        //Remove the key if count becomes zero
+        if(currCount==1){
+            mp.remove(key);
+        }else{
+            if(currNode.prev.count!=currCount-1){
+                addNode(currNode.prev,currCount-1);
+            }
+            currNode.prev.keys.add(key);
+            mp.put(key,currNode.prev);
+        }
+        currNode.keys.remove(key);
+        if(currNode.keys.isEmpty()){
+            removeNode(currNode);
+        }
+    }
+    
+    public String getMaxKey() {
+        if(last==head) return "";
+        return last.keys.get(0);
+    }
+    
     public String getMinKey() {
-        return set.isEmpty() ? "" : set.first().getValue();
+        if(head.next==null) return "";
+        return head.next.keys.get(0);
     }
 }
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne obj = new AllOne();
+ * obj.inc(key);
+ * obj.dec(key);
+ * String param_3 = obj.getMaxKey();
+ * String param_4 = obj.getMinKey();
+ */
