b3cf4b52511d21881a5ca91aac41fc5995aa0a3b
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-10-01 18:30:47 +0530

    Time: 3 ms (56.08%) | Memory: 45.1 MB (11.14%) - LeetSync

--- LeetCode-Problems/51-n-queens/n-queens.java
+++ LeetCode-Problems/51-n-queens/n-queens.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    boolean check(int n,List<String> board,int i,int j){
+    /*boolean check(int n,List<String> board,int i,int j){
         //upper left Diagonal, koi bhi queen present naa ho
         int row=i,col=j;
         while(row>=0 && col>=0){
@@ -18,31 +18,33 @@ class Solution {
             row--;
             col++;
         }
-        // for (row = 0; row < i; row++) {
-        //     if (board.get(row).charAt(j) == 'Q') {
-        //         return false;
-        //     }
-        // }
         return true;
-    }
-    void find(int row,int n,List<List<String>> ans,List<String> board,boolean[] col){
+    }*/
+    void find(int row,int n,List<List<String>> ans,List<String> board,boolean[] col,int[] leftDig,int[] rightDig){
         //Base condition
         if(row==n){
             ans.add(new ArrayList<>(board));
             return;
         }
         //put queen at any n position
+
+        //Left Dign: n-1+col-row
+        //right Dign: row+col
         for(int j=0;j<n;j++){
-            if(!col[j] && check(n,board,row,j)){
+            if(!col[j] &&leftDig[n-1+j-row]==0 && rightDig[row+j]==0){
                 col[j]=true;
                 char[] rowChars = board.get(row).toCharArray();
                 rowChars[j] = 'Q';
                 board.set(row, new String(rowChars));
-                find(row+1,n,ans,board,col);
+                leftDig[n-1+j-row]=1;
+                rightDig[row+j]=1;
+                find(row+1,n,ans,board,col,leftDig,rightDig);
                 
                 rowChars[j] = '.';
                 board.set(row, new String(rowChars));
                 col[j]=false;
+                leftDig[n-1+j-row]=0;
+                rightDig[row+j]=0;
             }
         }
 
@@ -57,8 +59,10 @@ class Solution {
             }
             board.add(s);
         }
+        int[] leftDig=new int[2*n-1];
+        int[] rightDig=new int[2*n-1];
         boolean[] col=new boolean[n];
-        find(0,n,ans,board,col);
+        find(0,n,ans,board, col, leftDig, rightDig);
         return ans;
     }
 }
