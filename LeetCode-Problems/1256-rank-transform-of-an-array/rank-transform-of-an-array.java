7a8a56bf5b60db3d57b48244202fa54587c18e7b
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-10-02 10:16:50 +0530

    Time: 81 ms (5.21%) | Memory: 67.3 MB (5.29%) - LeetSync

--- LeetCode-Problems/1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ LeetCode-Problems/1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -2,19 +2,14 @@ class Solution {
     public int[] arrayRankTransform(int[] arr) {
         TreeMap<Integer,Integer> map=new TreeMap<>();
         TreeSet<Integer> set=new TreeSet<>();
-        for(int i=0;i<arr.length;i++){
-            // map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-            set.add(arr[i]);
-        }
-        System.out.println(set);
+
+        for(int i=0;i<arr.length;i++) set.add(arr[i]);
+        
         int i=1;
-        for(int k:set){
-            map.put(k,i++);
-        }
-        System.out.println(map);
-        for(int j=0;j<arr.length;j++){
-            arr[j]=map.get(arr[j]);
-        }
+        for(int k:set) map.put(k,i++);
+
+        for(int j=0;j<arr.length;j++) arr[j]=map.get(arr[j]);
+        
         return arr;
     }
 }
