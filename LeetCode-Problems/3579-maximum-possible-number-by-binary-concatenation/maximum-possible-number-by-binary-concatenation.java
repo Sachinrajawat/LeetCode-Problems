4a2ad8b6e2a3af83ca4ec7e515a77a9b391811c6
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-10-06 23:29:16 +0530

    Time: 14 ms (100.00%) | Memory: 43.3 MB (100.00%) - LeetSync

---
+++ LeetCode-Problems/3579-maximum-possible-number-by-binary-concatenation/maximum-possible-number-by-binary-concatenation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxGoodNumber(int[] nums) {
+        String f=Integer.toBinaryString(nums[0]);
+        String s=Integer.toBinaryString(nums[1]);
+        String t=Integer.toBinaryString(nums[2]);
+        String ans=f+s+t;
+        System.out.println(ans);
+        int result=Integer.parseInt(ans,2);
+        ans=f+t+s;
+        result=Math.max(result,Integer.parseInt(ans,2));
+        ans=t+s+f;
+        result=Math.max(result,Integer.parseInt(ans,2));
+        ans=t+f+s;
+        result=Math.max(result,Integer.parseInt(ans,2));
+        ans=s+f+t;
+        result=Math.max(result,Integer.parseInt(ans,2));
+        ans=s+t+f;
+        result=Math.max(result,Integer.parseInt(ans,2));
+
+        return result;
+    }
+}
