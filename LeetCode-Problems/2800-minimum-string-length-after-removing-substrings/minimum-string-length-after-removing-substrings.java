0fddd7fc2fcef0d062eb18bedaaf8fc94e5233df
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-10-07 09:27:24 +0530

    Time: 28 ms (5.06%) | Memory: 45.3 MB (7.85%) - LeetSync

--- LeetCode-Problems/2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ LeetCode-Problems/2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -2,14 +2,16 @@ class Solution {
     public int minLength(String s) {
         boolean a=true;
         while(a){
-            if(s.contains("AB") || s.contains("CD")){
+            // if(s.contains("AB") || s.contains("CD")){
+                a=false;
                 for(int i=0;i<s.length()-1;i++){
                     if(s.substring(i,i+2).equals("AB") || s.substring(i,i+2).equals("CD")){
                         s=s.substring(0,i)+s.substring(i+2,s.length());
+                        a=true;
                     }
                 }
-            }
-            else break;
+            // }
+            // else break;
         }
         return s.length();
     }
