ce3645d73492505434818879e40d3508bdfb9004
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-10-07 09:27:48 +0530

    Time: 28 ms (5.06%) | Memory: 45.2 MB (10.81%) - LeetSync

--- LeetCode-Problems/2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ LeetCode-Problems/2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -2,7 +2,6 @@ class Solution {
     public int minLength(String s) {
         boolean a=true;
         while(a){
-            // if(s.contains("AB") || s.contains("CD")){
                 a=false;
                 for(int i=0;i<s.length()-1;i++){
                     if(s.substring(i,i+2).equals("AB") || s.substring(i,i+2).equals("CD")){
@@ -10,8 +9,6 @@ class Solution {
                         a=true;
                     }
                 }
-            // }
-            // else break;
         }
         return s.length();
     }
