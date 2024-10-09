a763cc1704229a5443a2c9b7ad84933591be8cdd
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-10-09 09:18:49 +0530

    Time: 1 ms (58.43%) | Memory: 41.1 MB (95.89%) - LeetSync

--- LeetCode-Problems/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ LeetCode-Problems/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,10 +1,7 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        int ans=0;
         Stack<Character> st=new Stack<>();
         for(char ch:s.toCharArray()){
-            // if(ch=='(') ans++;
-            // else if(ans>0) ans--;
             if(st.isEmpty()) st.push(ch);
             else{
                 if(ch==')' && st.peek()=='(') st.pop();
