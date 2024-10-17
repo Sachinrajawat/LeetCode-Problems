e51bc63b05b130eb75e1bf7ad657e086dd820bb5
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-10-17 14:31:36 +0530

    Time: 40 ms (64.78%) | Memory: 46 MB (71.10%) - LeetSync

--- LeetCode-Problems/416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ LeetCode-Problems/416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -7,11 +7,12 @@ class Solution {
             int n=nums.length;
             sum/=2;
             boolean[][] dp=new boolean[n+1][sum+1];
+            
+            for(int i=0;i<sum+1;i++){
+                dp[0][i]=false;
+            }
             for(int i=0;i<n+1;i++){
-                for(int j=0;j<sum+1;j++){
-                    if(i==0) dp[i][j]=false;
-                    if(i==j) dp[i][j]=true;
-                }
+                dp[i][0]=true;
             }
             for(int i=1;i<n+1;i++){
                 for(int j=1;j<sum+1;j++){
