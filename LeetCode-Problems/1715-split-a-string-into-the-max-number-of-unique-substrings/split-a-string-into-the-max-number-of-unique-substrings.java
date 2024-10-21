6d6f9bb49cc627ef0886cf25cca159dcf60e5adc
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-10-21 11:35:39 +0530

    Time: 43 ms (28.10%) | Memory: 44.7 MB (84.31%) - LeetSync

---
+++ LeetCode-Problems/1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int max=0;
+    public void solve(List<String> arr,String s,int start){
+        
+        if(start==s.length()){
+            max=Math.max(max,arr.size());
+            return;
+        }
+        for(int end=start+1;end<=s.length();end++){
+            String str=s.substring(start,end);
+            if(!arr.contains(str)){
+                arr.add(str);
+                solve(arr,s,end);
+                arr.remove(arr.size()-1);
+            }
+        }
+    }
+    public int maxUniqueSplit(String s) {
+        List<String> arr=new ArrayList<>();
+        solve(arr,s,0);
+        return max;
+    }
+}
