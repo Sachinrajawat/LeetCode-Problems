4b2024dc254987a02939f26e6580bf327b15a419
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-10-22 13:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (98.59%) - LeetSync

--- LeetCode-Problems/50-powx-n/powx-n.java
+++ LeetCode-Problems/50-powx-n/powx-n.java
@@ -1,13 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        if((x<1 &&x>0) && n>10000000) return 0;
         if((x==1 || x==-1) && n>10000000) return x;
         if(n<0){
             x=1/x;
             n=-n;
         }
-        if(n==1) return x;
-        double ans=x*myPow(x,n-1);
-        return ans;
+        return power(x,n);
+    }
+    public double power(double x,int n){
+        if(n==0) return 1;
+        double half=power(x,n/2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
     }
 }
