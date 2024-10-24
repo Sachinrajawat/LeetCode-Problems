76fbc01f02f5289d2de386b7fd8b1757bc872710
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-10-24 16:46:07 +0530

    Time: 460 ms (5.17%) | Memory: 54.9 MB (9.17%) - LeetSync

--- LeetCode-Problems/2107-find-unique-binary-string/find-unique-binary-string.java
+++ LeetCode-Problems/2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,10 +1,14 @@
 class Solution {
+    
     public void solve(ArrayList<String> arr,String[] s,String str,ArrayList<String> ans){
         if(str.length()>=arr.size()) return;
         int j=0;
         for(int i=0;i<2;i++){
             str+=s[i];
-            if(str.length()==arr.size() && (!arr.contains(str))) ans.add(str);
+            if(str.length()==arr.size() && (!arr.contains(str))){
+                ans.add(str);
+                break;
+            }
             solve(arr,s,str,ans);
             str=str.substring(0,str.length()-1);
         }
@@ -16,7 +20,6 @@ class Solution {
         String[] s={"0","1"};
         ArrayList<String> ans=new ArrayList<>();
         solve(arr,s,"",ans);
-        // System.out.println(ans);
         return ans.get(0);
     }
 }
