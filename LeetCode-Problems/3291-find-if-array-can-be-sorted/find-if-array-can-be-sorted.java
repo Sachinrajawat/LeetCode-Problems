55d75aa75cfe08493c82e9a0149f5b63bc3e8833
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-11-06 19:52:46 +0530

    Time: 14 ms (5.71%) | Memory: 44.7 MB (21.38%) - LeetSync

---
+++ LeetCode-Problems/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canSortArray(int[] nums) {
+        if(nums.length==1) return true;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            String s=Integer.toBinaryString(nums[i]);
+            int countOnes = s.length() - s.replace("1", "").length();
+            map.put(nums[i],countOnes);
+        }
+        for(int i=1;i<nums.length;i++){
+            for(int j=i;j>0;j--){
+                if(nums[j]>=nums[j-1]) break;
+                else{
+                    if(map.get(nums[j])!=map.get(nums[j-1])) return false;
+                    else{
+                        int temp=nums[j];
+                        nums[j]=nums[j-1];
+                        nums[j-1]=temp;
+                    }
+                }
+            }
+        }
+        // System.out.println(map);
+        return true;
+    }
+}
