013b3b3047dede9c0b9b120159766af0010b35c6
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-11-07 08:23:17 +0530

    Time: 4 ms (45.83%) | Memory: 44.4 MB (48.75%) - LeetSync

--- LeetCode-Problems/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ LeetCode-Problems/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -1,16 +1,11 @@
 class Solution {
     public boolean canSortArray(int[] nums) {
         if(nums.length==1) return true;
-        Map<Integer,Integer> map=new LinkedHashMap<>();
-        for(int i=0;i<nums.length;i++){
-            String s=Integer.toBinaryString(nums[i]);
-            int countOnes = s.length()-s.replace("1","").length();
-            map.put(nums[i],countOnes);
-        }
+        
         for(int i=nums.length-1;i>0;i--){
             for(int j=0;j<i;j++){
                 if(nums[j]>nums[j+1]){
-                    if(map.get(nums[j])!=map.get(nums[j+1])) return false;
+                    if(Integer.bitCount(nums[j])!=Integer.bitCount(nums[j+1])) return false;
                     else{
                         int temp=nums[j];
                         nums[j]=nums[j+1];
