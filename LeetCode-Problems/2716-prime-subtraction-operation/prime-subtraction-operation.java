2df4fb3492b4216d503011a8f64fbc12ab0be4b1
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-11-11 08:25:51 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (33.77%) - LeetSync

---
+++ LeetCode-Problems/2716-prime-subtraction-operation/prime-subtraction-operation.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPrime(int num){
+        if(num<=1) return false;
+        if(num<=3) return true;
+        if(num%2==0||num%3==0) return false;
+        for(int i=5;i*i<=num;i+=6){
+            if(num%i==0 || num%(i+2)==0) return false;
+        }
+        return true;
+    }
+    public int prime(int x,int num){
+        while(x<num){
+            if(isPrime(x)) return x;
+            x++;
+        }
+        return 0;
+    }
+    public boolean primeSubOperation(int[] nums) {
+        if(nums.length==1) return true;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]>=nums[i+1]){
+                int x=nums[i]-nums[i+1]+1;
+                int sub=prime(x,nums[i]);
+                if(sub==0) return false;
+                nums[i]-=sub;
+            }
+        }
+        // for(int i:nums) System.out.println(i);
+        return true;
+    }
+}
