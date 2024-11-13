f082af63271025f0a33ad597413cba062d5ffac0
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-11-13 16:06:56 +0530

    Time: 61 ms (54.35%) | Memory: 57.4 MB (42.09%) - LeetSync

--- LeetCode-Problems/2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
+++ LeetCode-Problems/2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -1,38 +1,29 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
         long count=0;
-        
         Arrays.sort(nums);
-        for(int i:nums) System.out.print(i+" ");
-        System.out.println();
         for(int i=0;i<nums.length-1;i++){
             int start=i+1,end=nums.length-1;
             int target=lower-nums[i];
-            int ans1=-1;
-            
+            int ans1=-1,ans2=-1;
             while(start<=end){
                 int mid=start+(end-start)/2;
                 if(nums[mid]>=target){
                     ans1=mid;
                     end=mid-1;
                 }
-                else{
-                    start=mid+1;
-                }
+                else start=mid+1;
             }
             target=upper-nums[i];
             start=i+1;
             end=nums.length-1;
-            int ans2=-1;
             while(start<=end){
                 int mid=start+(end-start)/2;
                 if(nums[mid]<=target){
                     ans2=mid;
                     start=mid+1;
                 }
-                else{
-                    end=mid-1;
-                }
+                else end=mid-1;
             }
             if (ans1 != -1 && ans2 != -1 && ans2 >= ans1) {
                 count += (ans2 - ans1 + 1);
