23e89e756823d843cc4313b59ffd4e3325ecbb0d
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-11-14 08:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (53.31%) - LeetSync

--- LeetCode-Problems/1013-fibonacci-number/fibonacci-number.java
+++ LeetCode-Problems/1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,16 @@
 class Solution {
-    public int find(int n){
+    public int find(int n,int[] dp){
         if(n<=1) return n;
-        return find(n-1)+find(n-2);
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=find(n-1,dp)+find(n-2,dp);
+        return dp[n];
     }
     public int fib(int n) {
-        return find(n);
+        if(n==0) return 0;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        dp[1]=1;
+        return find(n,dp);
     }
 }
