95062b7c7a2a4ec93a2e03d265950bbf70b989a1
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-11-16 14:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (12.53%) - LeetSync

--- LeetCode-Problems/213-house-robber-ii/house-robber-ii.java
+++ LeetCode-Problems/213-house-robber-ii/house-robber-ii.java
@@ -1,17 +1,21 @@
 class Solution {
-    public int find(int n,int[] nums,int index,int[] dp){
-        if(index>=n) return 0;
-        if(dp[index]!=-1) return dp[index];
-        dp[index]=Math.max(nums[index]+find(n,nums,index+2,dp),find(n,nums,index+1,dp));
-        return dp[index];
-    } 
+    
     public int rob(int[] nums) {
         int n=nums.length;
         if(n==1) return nums[0];
-        int[] dp1=new int[n];
-        Arrays.fill(dp1,-1);
-        int[] dp2=new int[n];
-        Arrays.fill(dp2,-1);
-        return Math.max(find(n-1,nums,0,dp1),find(n,nums,1,dp2));
+        int[] dp1=new int[n+2];
+        dp1[n]=0;
+        dp1[n-1]=0;
+        int[] dp2=new int[n+2];
+        dp2[n+1]=0;
+        dp2[n]=0;
+        for(int i=n-2;i>=0;i--){
+            dp1[i]=Math.max(nums[i]+dp1[i+2],dp1[i+1]);
+        }
+        for(int i=n-1;i>0;i--){
+            dp2[i]=Math.max(nums[i]+dp2[i+2],dp2[i+1]);
+        }
+
+        return Math.max(dp1[0],dp2[1]);
     }
 }
