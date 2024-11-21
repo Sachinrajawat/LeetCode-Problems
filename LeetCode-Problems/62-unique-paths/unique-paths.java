c43be74d5229fde1a2151fd7e1153ff93a4f889f
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-11-21 22:42:02 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (51.44%) - LeetSync

--- LeetCode-Problems/62-unique-paths/unique-paths.java
+++ LeetCode-Problems/62-unique-paths/unique-paths.java
@@ -1,26 +1,3 @@
-// class Solution {
-    // public int path(int m,int n,int i,int j,int[][] dp){
-    //     if(i==m || j==n) return 0;
-    //     if(i==m-1 || j==n-1) return 1;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     return dp[i][j]=path(m,n,i+1,j,dp)+path(m,n,i,j+1,dp);
-    // }
-//     public int uniquePaths(int m, int n) {
-//         int[][] dp=new int[m+1][n+1];
-//         for(int i=0;i<m;i++){
-//             dp[i][0]=1;
-//         }
-//         for(int i=0;i<n;i++){
-//             dp[0][i]=1;
-//         }
-//         for(int i=1;i<m+1;i++){
-//             for(int j=1;j<n+1;j++){
-//                 dp[i][j]=dp[i+1][j]+dp[i][j+1];
-//             }
-//         }
-//         return dp[m][n];
-//     }
-// }
 
 class Solution {
     public int path(int m,int n,int[][] dp){
