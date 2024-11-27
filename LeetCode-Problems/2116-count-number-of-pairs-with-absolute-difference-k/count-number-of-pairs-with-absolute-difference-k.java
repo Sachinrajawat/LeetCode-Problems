f5b7ba69bd7c14f413225be194165ab8239efcbe
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-11-27 21:11:20 +0530

    Time: 5 ms (68.24%) | Memory: 44.9 MB (6.65%) - LeetSync

--- LeetCode-Problems/2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
+++ LeetCode-Problems/2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
@@ -1,11 +1,19 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
-        int c=0;
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(Math.abs(nums[j]-nums[i])==k) c++;
+        var n = nums.length;
+        var ans = 0;
+
+        var counts = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            if (counts.containsKey(nums[i] - k)) {
+                ans += counts.get(nums[i] - k);
             }
+            if (counts.containsKey(k + nums[i])) {
+                ans += counts.get(k + nums[i]);
+            }
+            counts.put(nums[i], counts.getOrDefault(nums[i], 0) + 1);
         }
-        return c;
+
+    return ans;
     }
 }
