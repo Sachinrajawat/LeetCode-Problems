7b08085fe3a76f149b0af18026ff172ab5aa45ce
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-11-27 17:55:28 +0530

    Time: 5 ms (92.36%) | Memory: 44.7 MB (23.52%) - LeetSync

--- LeetCode-Problems/1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ LeetCode-Problems/1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -4,32 +4,21 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        TreeMap<Integer,ArrayList<Integer>> newMap=new TreeMap<>();
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->{
+            if(a[1]==b[1]) return b[0]-a[0];
+            else return a[1]-b[1];
+        });
         for(var key:map.keySet()){
-            if(newMap.containsKey(map.get(key))){
-                ArrayList<Integer> arr=newMap.get(map.get(key));
-                arr.add(key);
-                newMap.put(map.get(key),arr);
-            }
-            else{
-                ArrayList<Integer> arr=new ArrayList<>();
-                arr.add(key);
-                newMap.put(map.get(key),arr);
-            }
+            pq.add(new int[]{key,map.get(key)});
         }
         int j=0;
-        for(var key:newMap.keySet()){
-            ArrayList<Integer> arr=newMap.get(key);
-            Collections.sort(arr,Collections.reverseOrder());
-            while(arr.size()!=0){
-                for(int i=0;i<key;i++){
-                    System.out.print(j+" "+arr.get(0)+" ");
-                    nums[j++]=arr.get(0);
-                }
-                arr.remove(0);
+        int[] array=new int[nums.length];
+        while(!pq.isEmpty()){
+            int[] a=pq.poll();
+            for(int i=0;i<a[1];i++){
+                array[j++]=a[0];
             }
         }
-        System.out.println(newMap);
-        return nums;
+        return array;
     }
 }
