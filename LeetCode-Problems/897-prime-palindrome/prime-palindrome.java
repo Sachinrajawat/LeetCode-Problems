86c8114203dece1091a1801fe6294866fec5ae44
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-11-29 00:20:29 +0530

    Time: 1019 ms (20.48%) | Memory: 40.8 MB (32.72%) - LeetSync

--- LeetCode-Problems/897-prime-palindrome/prime-palindrome.java
+++ LeetCode-Problems/897-prime-palindrome/prime-palindrome.java
@@ -1,58 +1,33 @@
 class Solution {
-    public static int nextPalindrome(int num) {
-        String s = Integer.toString(num);
-        int len = s.length();
-        
-        // Handle odd and even length palindromes
-        String firstHalf = s.substring(0, (len + 1) / 2); // First half of the number
-        StringBuilder palindrome = new StringBuilder(firstHalf);
-        if (len % 2 != 0) {
-            palindrome.append(new StringBuilder(firstHalf).reverse().substring(1)); // Mirror and exclude middle
-        } else {
-            palindrome.append(new StringBuilder(firstHalf).reverse()); // Mirror completely
-        }
-        
-        int palin = Integer.parseInt(palindrome.toString());
-        if (palin >= num) {
-            return palin;
-        }
-
-        // If generated palindrome is less than the number, increment and generate again
-        int firstHalfNum = Integer.parseInt(firstHalf) + 1;
-        firstHalf = Integer.toString(firstHalfNum);
-        palindrome = new StringBuilder(firstHalf);
-        if (len % 2 != 0) {
-            palindrome.append(new StringBuilder(firstHalf).reverse().substring(1)); // Mirror and exclude middle
-        } else {
-            palindrome.append(new StringBuilder(firstHalf).reverse()); // Mirror completely
-        }
-        
-        return Integer.parseInt(palindrome.toString());
-    }
-
-    public static boolean isPrime(int n){
-        if(n<=1) return false;
-        if(n<=3) return true;
-        if(n%2==0 || n%3==0) return false;
-        for(int i=5;i*i<=n;i+=6) if(n%i==0||n%(i+2)==0) return false;
-        return true;
-    }
-    public int primePalindrome(int N) {
-        if (N <= 2) return 2; // Handle edge case for smallest prime palindrome
-
-        int candidate = N;
-        while (true) {
-            // Generate the next palindrome
-            candidate = nextPalindrome(candidate);
-            
-            // If it's a prime number, return it
-            if (isPrime(candidate)) {
-                return candidate;
+    public int primePalindrome(int n) {
+        int num=n;
+        while(n>0){
+            if(prime(num)==1 && palindrome(num)==1){
+                return num;
             }
-
-            // Move to the next potential palindrome
-            candidate++;
-
+            num++;
+            if(num >10000000 && num<100000000){
+                num=100000000;
+            }
+        }
+        return num;
     }
-}
+    public static int prime(int num){
+        if(num<=1) return 0;
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0) return 0;
+        }
+        return 1;
+    }
+    public static int palindrome(int num){
+        if(num<0) return 0;
+        int origin=num;
+        int rev=0;
+        while(num>0){
+            int rem=num%10;
+            rev=rev*10+rem;
+            num/=10;
+        }
+        return rev==origin?1:0;
+    }
 }
