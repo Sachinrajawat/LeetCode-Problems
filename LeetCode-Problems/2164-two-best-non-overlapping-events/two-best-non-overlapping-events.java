9ff6a4f7ea9d8b8291347c6a3f7132475d479149
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2024-12-08 19:24:49 +0530

    Time: 101 ms (5.40%) | Memory: 135.7 MB (9.14%) - LeetSync

---
+++ LeetCode-Problems/2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int binarySearch(int[][] events,int endTime,int n){
+        int l=0,r=n-1,result=n;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(events[mid][0]>endTime){
+                result=mid;
+                r=mid-1;
+            }
+            else l=mid+1;
+        }
+        return result;
+    }
+    public int solve(int[][] events,int idx,int count,int n,int[][] dp){
+        if(count==2 || idx>=n) return 0;
+        int nextValidIndex =binarySearch(events,events[idx][1],n);
+        if(dp[idx][count]!=-1) return dp[idx][count];
+        int take=events[idx][2]+solve(events,nextValidIndex,count+1,n,dp);
+        int not_take=solve(events,idx+1,count,n,dp);
+        return dp[idx][count]=Math.max(take,not_take);
+    }
+    public int maxTwoEvents(int[][] events) {
+        Arrays.sort(events,(a,b)->Integer.compare(a[0],b[0]));
+        int n=events.length;
+        int[][] dp=new int[n+1][3];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return solve(events,0,0,n,dp);
+    }
+}
