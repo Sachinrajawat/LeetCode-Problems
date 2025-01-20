77776e7d727d146727560128033425fca2e1d6eb
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-01-20 12:12:15 +0530

    Time: 123 ms (5.47%) | Memory: 66.7 MB (51.24%) - LeetSync

--- LeetCode-Problems/2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
+++ LeetCode-Problems/2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
@@ -1,33 +1,19 @@
 class Solution {
-    public boolean check(int[][] mat,int row,int col){
-        boolean ans=true;
-        for(int i=0;i<mat.length;i++){
-            if(mat[i][col]!=-1){
-                ans=false;
-                break;
-            }
-        }
-        if(!ans) ans=true;
-        else return ans;
-            for(int i=0;i<mat[0].length;i++){
-                if(mat[row][i]!=-1){
-                    ans=false;
-                    break;
-                }
-            }
-        return ans;
-    }
     public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int m=mat.length, n=mat[0].length;
         Map<Integer,int[]> map=new TreeMap<>();
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 map.put(mat[i][j],new int[]{i,j});
             }
         }
+        int[] row=new int[m];
+        int[] col=new int[n];
         for(int i=0;i<arr.length;i++){
             int[] a=map.get(arr[i]);
-            mat[a[0]][a[1]]=-1;
-            if(check(mat,a[0],a[1])) return i;
+            row[a[0]]++;
+            col[a[1]]++;
+            if(row[a[0]]==n || col[a[1]]==m) return i;
         }
         return 0;
     }
