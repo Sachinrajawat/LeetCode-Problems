7aa7bf8f5ef7e1c263df12d94b89e1c1a22e7bdf
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-01-21 10:26:50 +0530

    Time: 1 ms (94.26%) | Memory: 45.5 MB (64.19%) - LeetSync

--- LeetCode-Problems/724-find-pivot-index/find-pivot-index.java
+++ LeetCode-Problems/724-find-pivot-index/find-pivot-index.java
@@ -3,7 +3,6 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             nums[i]+=nums[i-1];
         }
-        for(int i=0;i<nums.length;i++) System.out.print(nums[i]+" ");
         for(int i=0;i<nums.length;i++){
             if(nums[nums.length-1]-nums[i]==(i==0?0:nums[i-1])) return i;
         }
