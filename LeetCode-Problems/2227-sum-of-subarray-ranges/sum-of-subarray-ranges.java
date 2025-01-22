c1317c0997f1fd1be2b29790fd72bf00720df0e4
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-01-22 11:50:37 +0530

    Time: 21 ms (47.52%) | Memory: 45.7 MB (5.16%) - LeetSync

--- LeetCode-Problems/2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ LeetCode-Problems/2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,15 +1,64 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        
+        int n=nums.length;
         long ans=0;
-        for(int i=0;i<nums.length;i++){
-            int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
-            for(int j=i;j<nums.length;j++){
-                max=Math.max(max,nums[j]);
-                min=Math.min(min,nums[j]);
-                ans+=max-min;
-                // System.out.println(max+" "+min+"  :"+ans);
+        int[] ng = new int[n]; // next greater
+        int[] pg = new int[n]; // previous greater
+        int[] ns = new int[n]; // next smaller
+        int[] ps = new int[n]; // previous smaller
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && nums[st.peek()]<nums[i]){
+                ng[st.pop()]=i;
             }
+            st.push(i);
+        }
+        while(!st.isEmpty()){
+            ng[st.pop()]=n;
+        }
+        st.clear(); 
+
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && nums[st.peek()]<=nums[i]){
+                pg[st.pop()] = i;
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()){
+            pg[st.pop()]=-1;
+        }
+        st.clear();
+
+
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && nums[st.peek()]>nums[i]){
+                ns[st.peek()]=i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()){
+            ns[st.peek()]=n;
+            st.pop();
+        }
+        st.clear();
+
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+                ps[st.peek()]=i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()){
+            ps[st.peek()]=-1;
+            st.pop();
+        }
+        for(int i=0;i<n;i++){
+            ans+=(long)nums[i]*(i-pg[i])*(ng[i]-i);
+            ans-=(long)nums[i]*(i-ps[i])*(ns[i]-i);
         }
         return ans;
     }
