34e53b7cc8d7d421f7535ab0b24188b3e154d56d
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-01-22 09:53:57 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (45.60%) - LeetSync

--- LeetCode-Problems/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ LeetCode-Problems/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -4,12 +4,10 @@ class Solution {
         int ans=-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]<target){
-                start=mid+1;
-            }
-            else if(nums[mid]>target){
-                end=mid-1;
-            }
+            if(nums[mid]<target) start=mid+1;
+            
+            else if(nums[mid]>target) end=mid-1;
+
             else{
                 ans=mid;
                 end=mid-1;
