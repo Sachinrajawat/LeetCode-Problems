c26b69e290ceeb81d71484b44088102275082d21
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-01-22 10:44:06 +0530

    Time: 1 ms (87.27%) | Memory: 41.1 MB (40.25%) - LeetSync

--- LeetCode-Problems/69-sqrtx/sqrtx.java
+++ LeetCode-Problems/69-sqrtx/sqrtx.java
@@ -1,5 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        long ans=0;
+        long st=0,end=x;
+        while(st<=end){
+            long mid=st+(end-st)/2;
+            if(mid*mid>x){
+                end=mid-1;
+            }
+            else{
+                ans=mid;
+                st=mid+1;
+            }  
+        }
+        return (int)ans;
     }
 }
