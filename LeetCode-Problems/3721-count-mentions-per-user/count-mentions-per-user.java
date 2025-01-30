67be5e7124f66accaaba3dad97cc02864448aa99
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-01-30 10:32:29 +0530

    Time: 26 ms (65.19%) | Memory: 45.6 MB (91.28%) - LeetSync

--- LeetCode-Problems/3721-count-mentions-per-user/count-mentions-per-user.java
+++ LeetCode-Problems/3721-count-mentions-per-user/count-mentions-per-user.java
@@ -4,18 +4,15 @@ class Solution {
             int timeA = Integer.parseInt(a.get(1));
             int timeB = Integer.parseInt(b.get(1));
             
-            // If times are different, sort by time
             if (timeA != timeB) return Integer.compare(timeA, timeB);
             
-            // If times are the same, give priority to "OFFLINE" over "MESSAGE"
             return a.get(0).equals("OFFLINE") ? -1 : 1;
         });
+
         int[] mentions=new int[numberOfUsers];
         ArrayList<Integer> online=new ArrayList<>();
-        ArrayList<Integer> offline=new ArrayList<>();
-        for(int i=0;i<numberOfUsers;i++){
-            online.add(i);
-        }
+        for(int i=0;i<numberOfUsers;i++) online.add(i);
+        
         int time=0;
         PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
         for(int i=0;i<events.size();i++){
@@ -23,15 +20,10 @@ class Solution {
             time=Integer.parseInt(l.get(1));
 
             while (!pq.isEmpty() && pq.peek()[0] <= time) {
-                int[] arr = pq.poll();  // Safely remove the top element
-                // System.out.println(arr[0]+" "+arr[1]);
+                int[] arr = pq.poll();
                 online.add(arr[1]);
             }
-            // System.out.println(l);
             if(l.get(0).equals("MESSAGE")){
-                
-                
-                // System.out.println(online);
                 if(l.get(2).equals("HERE")){
                     for(int j=0;j<online.size();j++){
                         mentions[online.get(j)]++;
