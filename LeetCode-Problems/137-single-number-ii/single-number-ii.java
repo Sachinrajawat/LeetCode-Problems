ab174b2c8ce9bc3e3e6af4a3c266177346274729
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-02-04 10:50:51 +0530

    Time: 11 ms (7.95%) | Memory: 45.5 MB (65.26%) - LeetSync

--- LeetCode-Problems/137-single-number-ii/single-number-ii.java
+++ LeetCode-Problems/137-single-number-ii/single-number-ii.java
@@ -3,21 +3,15 @@ class Solution {
         int[] count=new int[32];
         for(int i=0;i<nums.length;i++){
             String s=Integer.toBinaryString(nums[i]);
-            // System.out.println(s);
             for(int j=0;j<s.length();j++){
-                // System.out.println(s.charAt(s.length()-1-j));
                 if(s.charAt(s.length()-1-j)=='1') count[31-j]++;
-                // else count[31-j]++;
             }
         }
         String ans="";
         for(int i=0;i<32;i++){
-            if(count[i]%3!=0){
-                ans+='1';
-            }
+            if(count[i]%3!=0) ans+='1';
             else ans+='0';
         }
-        System.out.println(ans);
         return (int)Long.parseLong(ans,2);
     }
 }
