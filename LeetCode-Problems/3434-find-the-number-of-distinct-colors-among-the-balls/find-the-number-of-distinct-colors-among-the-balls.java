1fee02158f3800df4f64c79b21da2a24dab969f0
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-02-07 20:22:00 +0530

    Time: 45 ms (35.37%) | Memory: 106.8 MB (5.24%) - LeetSync

---
+++ LeetCode-Problems/3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] queryResults(int limit, int[][] queries) {
+        int[] ans=new int[queries.length];
+        HashMap<Integer,Integer> ballMap=new HashMap<>();
+        HashMap<Integer,ArrayList<Integer>> colMap=new HashMap<>();
+        for(int i=0;i<queries.length;i++){
+            int key=queries[i][0];
+            int val=queries[i][1];
+            if(ballMap.containsKey(key)){
+                ArrayList<Integer> array=colMap.get(ballMap.get(key));
+                array.remove((Integer) key);
+                if(array.size()==0) colMap.remove(ballMap.get(key));
+                ballMap.put(key,val);
+                if(colMap.containsKey(val)){
+                    ArrayList<Integer> arr=colMap.get(val);
+                    arr.add(key);
+                    colMap.put(val,arr);
+                }
+                else{
+                    ArrayList<Integer> arr=new ArrayList<>();
+                    arr.add(key);
+                    colMap.put(val,arr);
+                }
+            }
+            else{
+                ballMap.put(key,val);
+                if(colMap.containsKey(val)){
+                    ArrayList<Integer> arr=colMap.get(val);
+                    arr.add(key);
+                    colMap.put(val,arr);
+                }
+                else{
+                    ArrayList<Integer> arr=new ArrayList<>();
+                    arr.add(key);
+                    colMap.put(val,arr);
+                }
+            }
+            ans[i]=colMap.size();
+            // System.out.println(ballMap+" "+colMap);
+        }
+        return ans;
+    }
+}
