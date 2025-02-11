e7380ac389e0e76745b0d733fe3ce0fa2ef8d7d1
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-02-11 17:54:35 +0530

    Time: 5 ms (14.95%) | Memory: 45.8 MB (20.94%) - LeetSync

--- LeetCode-Problems/130-surrounded-regions/surrounded-regions.java
+++ LeetCode-Problems/130-surrounded-regions/surrounded-regions.java
@@ -6,111 +6,34 @@ class Solution {
        int m=board.length;
        int n=board[0].length;
        int[][] dir=new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(i==0 || j==0 || i==m-1 || j==n-1){
-                if(board[i][j]=='O'){
-                Queue<int[]> q=new LinkedList<>();
-                q.add(new int[]{i,j});
-                    board[i][j]='2';
-                    while(!q.isEmpty()){
-                        int[] arr=q.poll();
-                        int u=arr[0];
-                        int v=arr[1];
-                        for(int k=0;k<4;k++){
-                            if(isValid(u+dir[k][0],v+dir[k][1],m,n) && board[u+dir[k][0]][v+dir[k][1]]=='O'){
-                                q.add(new int[]{u+dir[k][0],v+dir[k][1]});
-                                board[u+dir[k][0]][v+dir[k][1]]='2';
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==m-1 || j==n-1){
+                    if(board[i][j]=='O'){
+                    Queue<int[]> q=new LinkedList<>();
+                    q.add(new int[]{i,j});
+                        board[i][j]='2';
+                        while(!q.isEmpty()){
+                            int[] arr=q.poll();
+                            int u=arr[0];
+                            int v=arr[1];
+                            for(int k=0;k<4;k++){
+                                if(isValid(u+dir[k][0],v+dir[k][1],m,n) && board[u+dir[k][0]][v+dir[k][1]]=='O'){
+                                    q.add(new int[]{u+dir[k][0],v+dir[k][1]});
+                                    board[u+dir[k][0]][v+dir[k][1]]='2';
+                                }
                             }
                         }
                     }
                 }
             }
-        }
        }
-    //    for(int j=0;j<n;j++){
-    //         if(board[0][j]=='O'){
-    //             Queue<int[]> q=new LinkedList<>();
-    //             q.add(new int[]{0,j});
-    //                 board[0][j]='2';
-    //                 while(!q.isEmpty()){
-    //                     int[] arr=q.poll();
-    //                     int u=arr[0];
-    //                     int v=arr[1];
-    //                     for(int i=0;i<4;i++){
-    //                         if(isValid(u+dir[i][0],v+dir[i][1],m,n) && board[u+dir[i][0]][v+dir[i][1]]=='O'){
-    //                             q.add(new int[]{u+dir[i][0],v+dir[i][1]});
-    //                             board[u+dir[i][0]][v+dir[i][1]]='2';
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         // 
-    //     for(int j=0;j<n;j++){
-    //         if(board[m-1][j]=='O'){
-    //             Queue<int[]> q=new LinkedList<>();
-    //             q.add(new int[]{m-1,j});
-    //                 board[m-1][j]='2';
-    //                 while(!q.isEmpty()){
-    //                     int[] arr=q.poll();
-    //                     int u=arr[0];
-    //                     int v=arr[1];
-    //                     for(int i=0;i<4;i++){
-    //                         if(isValid(u+dir[i][0],v+dir[i][1],m,n) && board[u+dir[i][0]][v+dir[i][1]]=='O'){
-    //                             q.add(new int[]{u+dir[i][0],v+dir[i][1]});
-    //                             board[u+dir[i][0]][v+dir[i][1]]='2';
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         // 
-    //     for(int j=1;j<m-1;j++){
-    //         if(board[j][0]=='O'){
-    //             Queue<int[]> q=new LinkedList<>();
-    //             q.add(new int[]{j,0});
-    //                 board[j][0]='2';
-    //                 while(!q.isEmpty()){
-    //                     int[] arr=q.poll();
-    //                     int u=arr[0];
-    //                     int v=arr[1];
-    //                     for(int i=0;i<4;i++){
-    //                         if(isValid(u+dir[i][0],v+dir[i][1],m,n) && board[u+dir[i][0]][v+dir[i][1]]=='O'){
-    //                             q.add(new int[]{u+dir[i][0],v+dir[i][1]});
-    //                             board[u+dir[i][0]][v+dir[i][1]]='2';
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         for(int j=1;j<m-1;j++){
-    //         if(board[j][n-1]=='O'){
-    //             Queue<int[]> q=new LinkedList<>();
-    //             q.add(new int[]{j,n-1});
-    //                 board[j][n-1]='2';
-    //                 while(!q.isEmpty()){
-    //                     int[] arr=q.poll();
-    //                     int u=arr[0];
-    //                     int v=arr[1];
-    //                     for(int i=0;i<4;i++){
-    //                         if(isValid(u+dir[i][0],v+dir[i][1],m,n) && board[u+dir[i][0]][v+dir[i][1]]=='O'){
-    //                             q.add(new int[]{u+dir[i][0],v+dir[i][1]});
-    //                             board[u+dir[i][0]][v+dir[i][1]]='2';
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
+    
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]=='2'){
-                    board[i][j]='O';
-                }
-                else if(board[i][j]=='O'){
-                    board[i][j]='X';
-                }
+                if(board[i][j]=='2') board[i][j]='O';
+                else if(board[i][j]=='O') board[i][j]='X';
             }
         }
-        }
     }
+}
