ce6e482473f7e4f98394e3a86816521da5135bc6
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-02-17 16:36:44 +0530

    Time: 925 ms (5.03%) | Memory: 50.6 MB (5.20%) - LeetSync

--- LeetCode-Problems/1160-letter-tile-possibilities/letter-tile-possibilities.java
+++ LeetCode-Problems/1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -1,22 +1,20 @@
 class Solution {
-    public void solve(boolean[] isVisited, String tiles,String s,HashSet<String> arr){
+    public void solve(boolean[] isVisited, String tiles,String s,HashSet<String> set){
         for(int i=0;i<tiles.length();i++){
             if(!isVisited[i]){
                 isVisited[i]=true;
-                solve(isVisited,tiles,s,arr);
-                arr.add(s+tiles.charAt(i));
-                solve(isVisited,tiles,s+tiles.charAt(i),arr);
+                solve(isVisited,tiles,s,set);
+                set.add(s+tiles.charAt(i));
+                solve(isVisited,tiles,s+tiles.charAt(i),set);
                 isVisited[i]=false;
             }
         }
         return;
     }
     public int numTilePossibilities(String tiles) {
-        // ArrayList<String> arr=new ArrayList<>();
-        HashSet<String> arr=new HashSet<>();
+        HashSet<String> set=new HashSet<>();
         boolean[] isVisited=new boolean[tiles.length()];
-        solve(isVisited,tiles,"",arr);
-        System.out.println(arr);
-        return arr.size();
+        solve(isVisited,tiles,"",set);
+        return set.size();
     }
 }
