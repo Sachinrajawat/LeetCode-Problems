c5546b8641af4e230d4599b5450badc1611215c3
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-02-28 20:02:24 +0530

    Time: 10 ms (59.17%) | Memory: 44.8 MB (35.83%) - LeetSync

--- LeetCode-Problems/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ LeetCode-Problems/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -17,12 +17,12 @@ class Solution {
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(mat[i][j]+" ");
-            }
-            System.out.println();
-        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         System.out.print(mat[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
         int ans=0,c=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
             int count=0;
