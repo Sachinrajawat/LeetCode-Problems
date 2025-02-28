24a8cdc233ed7b29740ccc4da314247d44d2fc4d
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-02-28 10:15:24 +0530

    Time: 257 ms (13.33%) | Memory: 55.9 MB (28.12%) - LeetSync

--- LeetCode-Problems/905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ LeetCode-Problems/905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -1,11 +1,12 @@
 class Solution {
-    public int solve1(int j,int k, int[] arr, HashMap<Integer, Integer> map){
+    public int solve1(int j,int k, int[] arr, HashMap<Integer, Integer> map,int[][] dp){
+        if(dp[j][k]!=-1) return dp[j][k];
         int target=arr[k]-arr[j];
         if(map.containsKey(target) && map.get(target)<j){
             int i=map.get(target);
-            return solve1(i,j,arr,map)+1;
+            return dp[j][k]=solve1(i,j,arr,map,dp)+1;
         }
-        return 2;
+        return dp[j][k]=2;
     }
     public int lenLongestFibSubseq(int[] arr) {
         HashMap<Integer,Integer> map=new HashMap<>();
@@ -13,9 +14,13 @@ class Solution {
             map.put(arr[i],i);
         }
         int res=0;
+        int[][] dp=new int[arr.length][arr.length];
+        for(int i=0;i<arr.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
         for(int j=1;j<arr.length-1;j++){
             for(int k=j+1;k<arr.length;k++){
-                int len=solve1(j,k,arr,map);
+                int len=solve1(j,k,arr,map,dp);
                 if(len>=3){
                     res=Math.max(res,len);
                 }
