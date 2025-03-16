025542fae6d43411b04edeb2636176454a10d471
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-03-16 20:36:20 +0530

    Time: 99 ms (23.61%) | Memory: 80.9 MB (11.87%) - LeetSync

---
+++ LeetCode-Problems/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solve(int[] prices,int idx,boolean hasStock, int count,int[][][] dp){
+        if(count==0 || idx>=prices.length) return 0;
+        int x=hasStock==true?1:0;
+        if(dp[idx][x][count]!=-1) return dp[idx][x][count];
+        int skip=solve(prices,idx+1,hasStock,count,dp);
+        int take=0;
+        if(!hasStock){
+            take=-prices[idx]+solve(prices,idx+1,true,count,dp);
+        }
+        else{
+            take=+prices[idx]+solve(prices,idx+1,false,count-1,dp);
+        }
+        return dp[idx][x][count]=Math.max(skip,take);
+    }
+    public int maxProfit(int[] prices) {
+        int[][][] dp=new int[prices.length][2][3];
+        for(int[][] arr:dp){
+            for(int[] array:arr){
+                Arrays.fill(array,-1);
+            }
+        }
+        return solve(prices,0,false,2,dp);
+    }
+}
