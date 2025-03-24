2589846bc658a9074417b4a22f01b272e01a3f03
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-03-24 20:47:40 +0530

    Time: 32 ms (21.29%) | Memory: 45.2 MB (24.35%) - LeetSync

--- LeetCode-Problems/322-coin-change/coin-change.java
+++ LeetCode-Problems/322-coin-change/coin-change.java
@@ -1,38 +1,20 @@
 class Solution {
-
-    int[] dp;
-    public int coinChange(int[] coins, int amount) {
-
-        dp = new int[amount + 1];
-        Arrays.fill(dp, -1);
-        int ans = coinCount(coins, amount);
-        return (ans == Integer.MAX_VALUE) ?  -1 : ans;
+    public int solve(int[] coins,int amount,int idx,int[][] dp){
+        if(amount==0) return 0;
+        if(idx<0) return Integer.MAX_VALUE;
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+        int skip=solve(coins,amount,idx-1, dp);
+        int pick=Integer.MAX_VALUE;
+        if(coins[idx]<=amount){
+            int res=solve(coins,amount-coins[idx],idx, dp);
+            if(res!=Integer.MAX_VALUE) pick=res+1;
+        }
+        return dp[idx][amount]=Math.min(pick,skip);
     }
-
-    int coinCount(int[] coins, int amount) {
-
-        if(amount == 0) {
-            return 0;
-        }
-        if(amount < 0) {
-            return Integer.MAX_VALUE;
-        }
-
-        if(dp[amount] != -1) {
-            return dp[amount];
-        }
-
-        int minCoins = Integer.MAX_VALUE;
-        for(int i = 0; i < coins.length; i++) {
-            int ans = coinCount(coins, amount - coins[i]);
-
-            if(ans != Integer.MAX_VALUE) {
-
-                //we have returned 0 in ans, so now we are updating the ans count
-                //hence 1 + ans
-                minCoins = Math.min(minCoins, 1 + ans);
-            }
-        }
-        return dp[amount] = minCoins;
+    public int coinChange(int[] coins, int amount) {
+        int[][] dp=new int[coins.length+1][amount+1];
+        for(int[] d:dp) Arrays.fill(d,-1);
+        int ans=solve(coins,amount,coins.length-1,dp);
+        return ans==2147483647?-1:ans;
     }
 }
