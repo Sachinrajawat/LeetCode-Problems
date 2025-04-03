b57650d520100e7b16c51d6f7be8be6af9b4f858
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-04-03 15:07:55 +0530

    Time: 1670 ms (6.75%) | Memory: 108.7 MB (99.60%) - LeetSync

---
+++ LeetCode-Problems/334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n=nums.length;
+        if(n<3) return false;
+        int[] prefMin=new int[n-2];
+        int min=nums[0];
+        for(int i=1;i<n-1;i++){
+            min=Math.min(min,nums[i]);
+            prefMin[i-1]=min;
+        }
+        int max=nums[n-1];
+        int[] sufMax=new int[n-2];
+        for(int i=n-1;i>1;i--){
+            max=Math.max(max,nums[i]);
+            sufMax[i-2]=max;
+        }
+        for(int j=1;j<n-1;j++){
+            System.out.println(prefMin[j-1]+" "+nums[j]+" "+sufMax[j-1]);
+            boolean left=false, right=false;
+            if(prefMin[j-1]<nums[j]) left=true;
+            if(sufMax[j-1]>nums[j]) right=true;
+            if(left&&right) return true;
+        }
+        return false;
+    }
+}
