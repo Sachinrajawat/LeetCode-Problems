3c693de8257a6d1bb637f8a455dc36a243bb874b
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-04-05 09:31:06 +0530

    Time: 14 ms (66.68%) | Memory: 42.7 MB (98.80%) - LeetSync

--- LeetCode-Problems/368-largest-divisible-subset/largest-divisible-subset.java
+++ LeetCode-Problems/368-largest-divisible-subset/largest-divisible-subset.java
@@ -12,7 +12,7 @@ class Solution {
         int lastIdx=0;
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<i;j++){
-                if(nums[i]>nums[j] && (nums[i]%nums[j]==0 || nums[j]%nums[i]==0) && dp[i]<dp[j]+1){
+                if(nums[i]>nums[j] && (nums[i]%nums[j]==0) && dp[i]<dp[j]+1){
                     dp[i]=dp[j]+1;
                     hash[i]=j;
                 }
