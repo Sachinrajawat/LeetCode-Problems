172ac8c5340f8bd1c72b819a00e174df1fce0fcf
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-04-10 22:28:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (94.93%) - LeetSync

--- LeetCode-Problems/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ LeetCode-Problems/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,35 +1,30 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int size = 0;
-        ListNode temp = head;
-        while (temp != null) {
-            temp = temp.next;
+        ListNode temp=head;
+        int size=0;
+        while(temp!=null){
+            temp=temp.next;
             size++;
         }
-        int m = size - n + 1;
-        temp = head;
-        for (int i = 1; i < m - 1; i++) {
-            temp = temp.next;
+        if(size==n) return head.next;
+        temp=head;
+        int m=size-n+1;
+        for(int i=1;i<m-1;i++){
+            temp=temp.next;
         }
-        //System.out.println(temp.val);
-        if(m==1)
-            head=head.next;
-        else if(size!=1 && temp.next.next!=null)
-            temp.next = temp.next.next;
-        else if(size==1)
-            head=null;
-        else
-            temp.next=null;
+        if(m==1) return null;
+
+        temp.next=temp.next.next;
         return head;
     }
 }
