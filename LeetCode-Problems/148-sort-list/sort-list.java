dd39d5ad78a8855297f7847444405c9a82237bdd
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-04-11 11:45:32 +0530

    Time: 9 ms (94.62%) | Memory: 56.3 MB (72.48%) - LeetSync

--- LeetCode-Problems/148-sort-list/sort-list.java
+++ LeetCode-Problems/148-sort-list/sort-list.java
@@ -9,20 +9,42 @@
  * }
  */
 class Solution {
+    public ListNode merger(ListNode left,ListNode right){
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp=dummyNode;
+        while(left!=null && right!=null){
+            if(left.val<right.val){
+                temp.next=left;
+                temp=left;
+                left=left.next;
+            }
+            else{
+                temp.next=right;
+                temp=right;
+                right=right.next;
+            }
+        }
+        if(left!=null){
+            temp.next=left;
+        }
+        else temp.next=right;
+        return dummyNode.next;
+    }
     public ListNode sortList(ListNode head) {
-        if(head==null) return null;
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        ListNode temp=head;
-        while(temp!=null){
-            pq.add(temp.val);
-            temp=temp.next;
+        if(head==null || head.next==null) return head;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        ListNode l=new ListNode(pq.poll());
-        temp=l;
-        while(!pq.isEmpty()){
-            l.next=new ListNode(pq.poll());
-            l=l.next;
-        }
-        return temp;
+        ListNode right=slow.next;
+        slow.next=null;
+        ListNode left=head;
+        left=sortList(left);
+        right=sortList(right);
+
+       
+        return merger(left,right);
     }
 }
