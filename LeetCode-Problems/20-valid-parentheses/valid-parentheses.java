c25a3ba461f1a28763085d9514ddc04346ed6565
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-04-13 17:37:13 +0530

    Time: 3 ms (41.90%) | Memory: 41.8 MB (66.22%) - LeetSync

--- LeetCode-Problems/20-valid-parentheses/valid-parentheses.java
+++ LeetCode-Problems/20-valid-parentheses/valid-parentheses.java
@@ -1,31 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st=new Stack<>();
-        int size=s.length();
-        if(size%2!=0) return false;
-        for(int i=0;i<size;i++){
-            char ch=s.charAt(i);
-            if(ch=='('||ch=='{'||ch=='['){
+        for(char ch:s.toCharArray()){
+            if(st.isEmpty()) st.push(ch);
+            else if(ch=='(' || ch=='[' || ch=='{'){
                 st.push(ch);
             }
             else{
-                if(st.size()==0) return false;
-                char d=st.peek();
-                if(ch==']'){
-                    if(d=='[') st.pop();
-                    else return false;
+                if(st.peek()=='[' && ch==']' || st.peek()=='(' && ch==')' || st.peek()=='{' && ch=='}'){
+                    st.pop();
                 }
-                else if(ch==')'){
-                    if(d=='(') st.pop();
-                    else return false;
-                }
-                else{
-                    if(d=='{') st.pop();
-                    else return false;
-                }
+                else return false;
             }
         }
-        if(st.size()>0) return false;
-        else return true;
+        return st.isEmpty();
     }
 }
