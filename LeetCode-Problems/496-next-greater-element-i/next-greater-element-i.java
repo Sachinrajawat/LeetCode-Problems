d680d09b1fe821bef885868ecc2eb151aa7569a8
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-04-14 15:45:44 +0530

    Time: 3 ms (89.15%) | Memory: 44.3 MB (37.31%) - LeetSync

--- LeetCode-Problems/496-next-greater-element-i/next-greater-element-i.java
+++ LeetCode-Problems/496-next-greater-element-i/next-greater-element-i.java
@@ -3,23 +3,16 @@ class Solution {
         Stack<Integer> st=new Stack<>();
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=nums2[i]){
+                st.pop();
+            }
             if(st.isEmpty()){
                 map.put(nums2[i],-1);
-                st.push(nums2[i]);
             }
             else{
-                if(st.peek()>nums2[i]){
-                    map.put(nums2[i],st.peek());
-                    st.push(nums2[i]);
-                }else{
-                    while(!st.isEmpty() && st.peek()<=nums2[i]){
-                        st.pop();
-                    }
-                    if(st.isEmpty()) map.put(nums2[i],-1);
-                    else map.put(nums2[i],st.peek());
-                    st.push(nums2[i]);
-                }
+                map.put(nums2[i],st.peek());
             }
+            st.push(nums2[i]);
         }
         for(int i=0;i<nums1.length;i++){
             nums1[i]=map.get(nums1[i]);
