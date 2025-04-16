74baa07241161e57ba311666c1f87b69a2777b09
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-04-16 11:37:22 +0530

    Time: 1 ms (18.29%) | Memory: 41.2 MB (52.53%) - LeetSync

--- LeetCode-Problems/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ LeetCode-Problems/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -14,47 +14,21 @@
  * }
  */
 class Solution {
-    public ArrayList<ArrayList<Integer>> path(TreeNode root,ArrayList<ArrayList<Integer>> list,ArrayList<Integer> arr){
-        if(root.left==null &&root.right==null){
-            list.add(arr);
-            return list;
-        }
+    public void solve(TreeNode root,String curr,int[] arr){
         
-        if(root.left!=null && root.right!=null){
-            ArrayList<Integer> aL= new ArrayList<>(arr);
-            aL.add(root.left.val);
-            path(root.left,list,aL);
-            ArrayList<Integer> aR= new ArrayList<>(arr);
-            aR.add(root.right.val);
-            path(root.right,list,aR);
+        if(root.left==null && root.right==null){
+            String s=String.valueOf(root.val);
+            curr+=s;
+            arr[0]+=Integer.parseInt(curr);
+            return;
         }
-        else if(root.left!=null || root.right!=null){
-            if(root.left!=null){
-                arr.add(root.left.val);
-                path(root.left,list,arr);
-            }
-            else if(root.right!=null){
-                arr.add(root.right.val);
-                path(root.right,list,arr);
-            }
-        }
-        return list;
+        String s=String.valueOf(root.val);
+        if(root.left!=null) solve(root.left,curr+s,arr);
+        if(root.right!=null) solve(root.right,curr+s,arr);
     }
     public int sumNumbers(TreeNode root) {
-        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
-        ArrayList<Integer> arr=new ArrayList<>();
-        arr.add(root.val);
-        path(root,list,arr);
-        int sum=0;
-        for(int i=0;i<list.size();i++){
-            ArrayList<Integer> a=list.get(i);
-            int pathSum=0;
-            int n=a.size()-1;
-            for(int j=0;j<a.size();j++){
-                pathSum+=a.get(j)*(int)Math.pow(10,n-j);
-            }
-            sum+=pathSum;
-        }
-        return sum;
+        int[] arr={0};
+        solve(root,"",arr);
+        return arr[0];
     }
 }
