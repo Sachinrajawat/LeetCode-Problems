a9918b459444d1d1344a11538eed0c38fa3cb9a7
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-04-23 10:38:28 +0530

    Time: 16 ms (96.93%) | Memory: 55.7 MB (70.03%) - LeetSync

--- LeetCode-Problems/948-sort-an-array/sort-an-array.java
+++ LeetCode-Problems/948-sort-an-array/sort-an-array.java
@@ -1,39 +1,6 @@
 class Solution {
-    public void divide(int[] nums,int left,int right){
-        if(left==right) return;
-            int mid=(left+right)/2;
-            divide(nums,left,mid);
-            divide(nums,mid+1,right);
-            merge(nums,left,mid,right);
-        
-    }
-    void merge(int[] nums,int left,int mid,int right){
-        int n1=mid-left+1;
-        int n2=right-mid;
-        int[] leftArray=new int[n1];
-        int[] rightArray=new int[n2];
-        for(int i=0;i<n1;i++){
-            leftArray[i]=nums[i+left];
-        }
-        for(int i=0;i<n2;i++){
-            rightArray[i]=nums[mid+i+1];
-        }
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            if (leftArray[i] < rightArray[j])
-                nums[k++] = leftArray[i++];
-            else
-                nums[k++] = rightArray[j++];
-        }
-        while (i < n1) {
-            nums[k++] = leftArray[i++];
-        }
-        while (j < n2) {
-            nums[k++] = rightArray[j++];
-        }
-    }
     public int[] sortArray(int[] nums) {
-        divide(nums,0,nums.length-1);
+        Arrays.sort(nums);
         return nums;
     }
 }
