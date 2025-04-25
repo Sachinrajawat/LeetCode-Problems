8b2b47fb92956e52454e214665582c842d09cbf9
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-04-25 19:48:03 +0530

    Time: 37 ms (36.70%) | Memory: 62.6 MB (65.14%) - LeetSync

--- LeetCode-Problems/2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
+++ LeetCode-Problems/2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
@@ -1,18 +1,13 @@
 class Solution {
     public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
         int n=nums.size();
-        long ans=0;
-        int[] count=new int[n];
-        for(int i=0;i<n;i++){
-            if(nums.get(i)%modulo==k)
-            count[i]=1;
-        }
-        long sum=0;
+        long ans=0, sum=0;
         Map<Integer,Long> map=new HashMap<>();
         map.put(0,1L);
         for(int i=0;i<n;i++){
-            
-            sum+=count[i];
+            if(nums.get(i) % modulo == k) {
+                sum += 1;
+            }
             int r1=(int)sum%modulo;
             int r2=(r1-k+modulo)%modulo;
             ans+=map.getOrDefault(r2,0L);
