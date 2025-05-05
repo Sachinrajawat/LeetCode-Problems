c23bab8ba5f58588dc5bc71ca2d42078345c1b3d
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-05-05 19:02:05 +0530

    Time: 2 ms (31.99%) | Memory: 45 MB (78.89%) - LeetSync

--- LeetCode-Problems/801-is-graph-bipartite/is-graph-bipartite.java
+++ LeetCode-Problems/801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,28 +1,27 @@
 class Solution {
-    public boolean dfs(int[][] graph,int[] color,int idx){
-        // boolean ans=true;
-        for(int i=0;i<graph[idx].length;i++){
-            if(color[graph[idx][i]]==-1){
-                color[graph[idx][i]]=(color[idx]+1)%2;
-                if(!dfs(graph,color,graph[idx][i])) return false;
-            }
-            else{
-                if(color[idx]==color[graph[idx][i]]) return false;
-            }
-        }
-        return true;
-    }
     public boolean isBipartite(int[][] graph) {
         int n=graph.length;
         int[] color=new int[n];
         Arrays.fill(color,-1);
-         for (int i=0;i<n;i++) {
-            if (color[i]==-1) { 
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                q.add(i);
                 color[i]=0;
-                if (!dfs(graph, color, i)) return false;
+                while(!q.isEmpty()){
+                    int x=q.remove();
+                    int[] arr=graph[x];
+                    for(int j=0;j<arr.length;j++){
+                        if(color[arr[j]]==color[x]) return false;
+                        if(color[arr[j]]==-1){
+                            if(color[x]==0) color[arr[j]]=1;
+                            else color[arr[j]]=0;
+                            q.add(arr[j]);
+                        }
+                    }
+                }
             }
         }
-         
         return true;
     }
 }
