16e379146b13293583491c80470852cd0496f739
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2025-06-01 20:33:40 +0530

    Time: 18 ms (58.82%) | Memory: 41.3 MB (14.71%) - LeetSync

---
+++ LeetCode-Problems/3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        int minCh1=Math.max(0,n-2*limit), maxCh1=Math.min(n,limit);
+        long ways=0;
+        for(int i=minCh1;i<=maxCh1;i++){
+            int N=n-i;
+            int minCh2=Math.max(0,N-limit);
+            int maxCh2=Math.min(N,limit);
+
+            ways+=maxCh2-minCh2+1;
+        }
+        return ways;
+    }
+}
