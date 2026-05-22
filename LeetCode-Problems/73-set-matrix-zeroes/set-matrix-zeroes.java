07ffb53288f69bac9c9e4dfad9e550eca6ae5dd3
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-05-22 21:01:13 +0530

    Time: 1 ms (98.34%) | Memory: 47.9 MB (12.63%) - LeetSync

--- LeetCode-Problems/73-set-matrix-zeroes/set-matrix-zeroes.java
+++ LeetCode-Problems/73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,27 +1,39 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         int n=matrix.length, m=matrix[0].length;
-        HashSet<Integer> row=new HashSet<>();
-        HashSet<Integer> col=new HashSet<>();
-        // System.out.println(n+" "+m);
+        boolean firstRowZero=false, firstColZero=false;
+        for(int i=0;i<m;i++){
+            if(matrix[0][i]==0){
+               firstRowZero=true;
+               break;
+            }
+        }
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+            if(matrix[i][0]==0){
+               firstColZero=true;
+               break;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(matrix[i][j]==0){
-                    row.add(i);
-                    col.add(j);
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
         }
-        // System.out.println(row+" "+col);
-        for(var r:row){
-            for(int j=0;j<m;j++){
-                matrix[r][j]=0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
             }
         }
-        for(var c:col){
-            for(int i=0;i<n;i++){
-                matrix[i][c]=0;
-            }
+        if(firstRowZero){
+            for(int i=0;i<m;i++) matrix[0][i]=0;
+        }
+        if(firstColZero){
+            for(int j=0;j<n;j++) matrix[j][0]=0;
         }
     }
 }
