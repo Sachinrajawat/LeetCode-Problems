fc9f8cb80af12bdffd83a220ac8f07df63739066
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-05-22 20:40:39 +0530

    Time: 3 ms (21.12%) | Memory: 47.7 MB (43.04%) - LeetSync

--- LeetCode-Problems/73-set-matrix-zeroes/set-matrix-zeroes.java
+++ LeetCode-Problems/73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,20 +1,27 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        int n=matrix.length, m=matrix[0].length;
         HashSet<Integer> row=new HashSet<>();
         HashSet<Integer> col=new HashSet<>();
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
+        // System.out.println(n+" "+m);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
                     row.add(i);
                     col.add(j);
                 }
             }
         }
-        for(var val:row){
-            for(int i=0;i<matrix[0].length;i++) matrix[val][i]=0;
+        // System.out.println(row+" "+col);
+        for(var r:row){
+            for(int j=0;j<m;j++){
+                matrix[r][j]=0;
+            }
         }
-        for(var val:col){
-            for(int i=0;i<matrix.length;i++) matrix[i][val]=0;
+        for(var c:col){
+            for(int i=0;i<n;i++){
+                matrix[i][c]=0;
+            }
         }
     }
 }
