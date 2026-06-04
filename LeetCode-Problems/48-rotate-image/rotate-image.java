93cea98b3301aac3d1ff902db796f59af44faa67
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-04 11:48:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (50.35%) - LeetSync

--- LeetCode-Problems/48-rotate-image/rotate-image.java
+++ LeetCode-Problems/48-rotate-image/rotate-image.java
@@ -1,29 +1,15 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length-1;
-        /*int[][] nMatrix=new int[n][n];
-        int k=matrix[0].length-1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                nMatrix[j][k]=matrix[i][j];
+        int row=matrix.length, col=matrix[0].length;
+        int[][] mat=new int[row][col];
+        for(int i=0;i<col;i++){
+            for(int j=0;j<row;j++){
+                mat[j][col-i-1]=matrix[i][j];
             }
-            k--;
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(nMatrix[i][j]+" ");
-            }
-            System.out.println();
-        }*/
-        for(int i=0;i<n+1/2;i++){
-            for(int j=i;j<n-i;j++){
-                int x=matrix[j][n-i];
-                matrix[j][n-i]=matrix[i][j];
-                int y=matrix[n-i][n-j];
-                matrix[n-i][n-j]=x;
-                x=matrix[n-j][i];
-                matrix[n-j][i]=y;
-                matrix[i][j]=x;
+        for(int i=0;i<col;i++){
+            for(int j=0;j<row;j++){
+                matrix[i][j]=mat[i][j];
             }
         }
     }
