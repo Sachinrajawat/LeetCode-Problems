1304a5c15e78e3f191e5d97b6b591854759d0450
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-04 22:08:14 +0530

    Time: 13 ms (29.66%) | Memory: 52.8 MB (74.24%) - LeetSync

--- LeetCode-Problems/169-majority-element/majority-element.java
+++ LeetCode-Problems/169-majority-element/majority-element.java
@@ -1,12 +1,12 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n=nums.length;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
         for(var key:map.keySet()){
-            if(map.get(key)>n/2) return key;
+            if(map.get(key)>(n/2)) return key;
         }
         return 0;
     }
