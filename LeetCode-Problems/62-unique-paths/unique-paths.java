18dbf62ee59380f9993d25454e2a065f6c97eb51
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-05 15:34:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (90.52%) - LeetSync

--- LeetCode-Problems/62-unique-paths/unique-paths.java
+++ LeetCode-Problems/62-unique-paths/unique-paths.java
@@ -1,14 +1,17 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        
-        int[] dp=new int[n];
-        Arrays.fill(dp,1);
-        int prev=0;
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[j]=dp[j]+dp[j-1];
-            }
+    public int ans(int i, int j, int[][] dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=ans(i-1,j, dp);
+        int left=ans(i,j-1,dp);
+        return dp[i][j]=up+left;
         }
-        return dp[n-1];
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        return ans(m-1, n-1, dp);
     }
 }
