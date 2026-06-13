91b9b478f70b903cdd5450d66dbbce12b3248f36
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-13 19:12:06 +0530

    Time: 37 ms (10.10%) | Memory: 47.4 MB (11.20%) - LeetSync

--- LeetCode-Problems/322-coin-change/coin-change.java
+++ LeetCode-Problems/322-coin-change/coin-change.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int solve(int[] coins,int amount,int idx,int[][] dp){
+    public int solve(int[] coins, int amount, int[][] dp, int idx){
         if(amount==0) return 0;
         if(idx<0) return Integer.MAX_VALUE;
         if(dp[idx][amount]!=-1) return dp[idx][amount];
-        int skip=solve(coins,amount,idx-1, dp);
+        int skip=solve(coins, amount,dp,idx-1);
         int pick=Integer.MAX_VALUE;
         if(coins[idx]<=amount){
-            int res=solve(coins,amount-coins[idx],idx, dp);
+            int res=solve(coins, amount-coins[idx], dp, idx);
             if(res!=Integer.MAX_VALUE) pick=res+1;
         }
-        return dp[idx][amount]=Math.min(pick,skip);
+        return dp[idx][amount]=Math.min(pick, skip);
     }
     public int coinChange(int[] coins, int amount) {
         int[][] dp=new int[coins.length+1][amount+1];
-        for(int[] d:dp) Arrays.fill(d,-1);
-        int ans=solve(coins,amount,coins.length-1,dp);
+        for(int[] d:dp) Arrays.fill(d, -1);
+        int ans=solve(coins, amount, dp, coins.length-1);
         return ans==2147483647?-1:ans;
     }
 }
