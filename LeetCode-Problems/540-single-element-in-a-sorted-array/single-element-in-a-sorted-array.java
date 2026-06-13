538486191d1636f96daa42886c60125f0afd7e7d
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-13 23:30:25 +0530

    Time: 1 ms (24.37%) | Memory: 52.9 MB (37.27%) - LeetSync

--- LeetCode-Problems/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ LeetCode-Problems/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,7 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int ans=0;
-        for(int i=0;i<nums.length;i++){
-            ans^=nums[i];
-        }
+        for(int i=0;i<nums.length;i++) ans^=nums[i];
         return ans;
     }
 }
