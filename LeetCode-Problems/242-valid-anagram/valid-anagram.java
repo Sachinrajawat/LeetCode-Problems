fb11916efd941f29f8cd32c8d7ed60d8c67c7ae2
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-14 21:32:36 +0530

    Time: 3 ms (91.96%) | Memory: 46.7 MB (15.44%) - LeetSync

---
+++ LeetCode-Problems/242-valid-anagram/valid-anagram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChar=s.toCharArray();
+        char[] tChar=t.toCharArray();
+        Arrays.sort(sChar);
+        Arrays.sort(tChar);
+        String sStr=new String(sChar);
+        String tStr=new String(tChar);
+        return sStr.equals(tStr);
+    }
+}
