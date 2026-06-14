b495b90f571402fed5c3db6ad7f0c2a3f1cc5bfc
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-14 14:30:16 +0530

    Time: 7 ms (5.42%) | Memory: 44.8 MB (81.66%) - LeetSync

--- LeetCode-Problems/496-next-greater-element-i/next-greater-element-i.java
+++ LeetCode-Problems/496-next-greater-element-i/next-greater-element-i.java
@@ -9,13 +9,11 @@ class Solution {
                     break;
                 }
             }
-            // System.out.println(nge[i]);
         }
         int[] ans=new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
             for(int j=0;j<nums2.length;j++){
                 if(nums1[i]==nums2[j]){
-                    // System.out.print(j+"   "+nge[j]+" ");
                     ans[i]=nge[j];
                 }
             }
