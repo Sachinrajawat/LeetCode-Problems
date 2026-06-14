2db595cd3e450aba692bb7180b48209dfabf83d6
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-14 17:59:11 +0530

    Time: 574 ms (12.92%) | Memory: 46.6 MB (39.11%) - LeetSync

--- LeetCode-Problems/5-longest-palindromic-substring/longest-palindromic-substring.java
+++ LeetCode-Problems/5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,36 +1,26 @@
-public class Solution {
-    public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
+class Solution {
+    public boolean isPalindrome(String s){
+        int i=0, j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
         }
-
-        int maxLen = 1;
-        String maxStr = s.substring(0, 1);
-
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + maxLen; j <= s.length(); j++) {
-                if (j - i > maxLen && isPalindrome(s.substring(i, j))) {
-                    maxLen = j - i;
-                    maxStr = s.substring(i, j);
+        return true;
+    }
+    public String longestPalindrome(String s) {
+        if(s.length()==1) return s;
+        int maxLen=1;
+        String maxStr = s.substring(0,1);
+        for(int i=0;i<s.length();i++){
+            for(int j=i+maxLen;j<=s.length();j++){
+                if(j-i>maxLen && isPalindrome(s.substring(i,j))){
+                    maxLen=j-i;
+                    maxStr=s.substring(i,j);
                 }
             }
         }
-
         return maxStr;
-    }
 
-    private boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
     }
 }
