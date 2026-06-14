db1a33df60b10550bdf48ad19bb4263a8ef8e63f
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-14 10:33:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (45.91%) - LeetSync

--- LeetCode-Problems/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ LeetCode-Problems/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,22 +1,28 @@
 class Solution {
-    int find(int[] nums,int st, int end, int target){
+    public int search(int[] nums, int target) {
+        int idx=-1, n=nums.length;
+        if(n==1) return nums[0]==target?0:-1;
+        if(nums[0]==target) return 0;
+        if(nums[n-1]==target) return n-1;
+        int st=1, end=n-2;
         while(st<=end){
             int mid=st+(end-st)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[mid]<target) st=mid+1;
-            else end=mid-1;
-        }
-        return -1;
-    }
-    public int search(int[] nums, int target) {
-        int mid=-1, n=nums.length;
-        for(int i=0;i<n-1;i++){
-            if(nums[i]>nums[i+1]){
-                mid=i;
-                break;
+            if(nums[st]<=nums[mid]){
+                if(nums[st]<=target && target<nums[mid]){
+                    end=mid-1;
+                }
+                else{
+                    st=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && target<=nums[end]){
+                    st=mid+1;
+                }
+                else end=mid-1;
             }
         }
-        int ans=find(nums,0,mid,target);
-        return ans==-1?find(nums,mid+1,n-1,target):ans;
+        return -1;
     }
 }
