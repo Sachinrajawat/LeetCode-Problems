5134dad10fb4aff305e4da5f3b9344b08cc74c66
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-14 23:32:55 +0530

    Time: 1 ms (80.84%) | Memory: 43.1 MB (26.41%) - LeetSync

--- LeetCode-Problems/165-compare-version-numbers/compare-version-numbers.java
+++ LeetCode-Problems/165-compare-version-numbers/compare-version-numbers.java
@@ -1,20 +1,26 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] ver1=version1.split("\\.");
-        String[] ver2=version2.split("\\.");
-        for(int i=0;i<Math.min(ver1.length,ver2.length);i++){
-            int v1=Integer.parseInt(ver1[i]);
-            int v2=Integer.parseInt(ver2[i]);
-            if(v1<v2) return -1;
-            else if(v1>v2) return 1;
+        String[] v1=version1.split("\\.");
+        String[] v2=version2.split("\\.");
+        int i=0, j=0;
+        while(i<v1.length && j<v2.length){
+            int val1=Integer.parseInt(v1[i]);
+            int val2=Integer.parseInt(v2[i]);
+            // System.out.println(val1+" "+val2);
+            if(val1>val2) return 1;
+            else if(val1<val2) return -1;
+            i++;
+            j++;
         }
-        if(ver1.length>ver2.length){
-            for(int i=ver2.length;i<ver1.length;i++)
-                if(Integer.parseInt(ver1[i])>0) return 1;
+        while(i<v1.length){
+            int val1=Integer.parseInt(v1[i]);
+            if(val1>0) return 1;
+            i++;
         }
-        else if(ver1.length<ver2.length){
-            for(int i=ver1.length;i<ver2.length;i++)
-                if(Integer.parseInt(ver2[i])>0) return -1;
+        while(j<v2.length){
+            int val2=Integer.parseInt(v2[j]);
+            if(val2>0) return -1;
+            j++;
         }
         return 0;
     }
