249bac70723d0131255160885566a8c83e3cc2d7
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-06-15 16:43:03 +0530

    Time: 350 ms (7.03%) | Memory: 48.1 MB (9.05%) - LeetSync

--- LeetCode-Problems/55-jump-game/jump-game.java
+++ LeetCode-Problems/55-jump-game/jump-game.java
@@ -1,17 +1,15 @@
 class Solution {
-    public boolean solve(int[] nums,int idx,Boolean[] dp){
+    public boolean solve(int[] nums, int idx, Boolean[] dp){
         if(idx>=nums.length) return false;
         if(idx==nums.length-1) return true;
-        if (dp[idx] != null) return dp[idx];
+        if(dp[idx]!=null) return dp[idx];
         for(int i=1;i<=nums[idx];i++){
-             if (solve(nums, idx + i, dp)) {
-                return dp[idx] = true;
-            }
+            if(solve(nums,idx+i, dp)) return dp[idx]=true;
         }
         return dp[idx]=false;
     }
     public boolean canJump(int[] nums) {
         Boolean[] dp=new Boolean[nums.length+1];
-        return solve(nums,0,dp);
+        return solve(nums,0, dp);
     }
 }
