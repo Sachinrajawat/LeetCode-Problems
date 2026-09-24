1040ae148e34ec1f69ab24aa35db96a81236f0ed
Sachin Singh Rajawat <120497065+Sachinrajawat@users.noreply.github.com>
2026-09-24 23:12:51 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (90.98%) - LeetSync

--- LeetCode-Problems/268-missing-number/missing-number.java
+++ LeetCode-Problems/268-missing-number/missing-number.java
@@ -1,14 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int ans=0;
-        for(int i=1;i<=nums.length;i++){
-            ans^=i;
-            
-        }
+        int[] arr=new int[nums.length+1];
         for(int i=0;i<nums.length;i++){
-            ans^=nums[i];
-            
+            arr[nums[i]]=-1;
         }
-        return ans;
+        for(int i=0;i<nums.length+1;i++){
+            if(arr[i]!=-1) return i;
+        }
+        return 0;
     }
 }
